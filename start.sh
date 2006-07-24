26207ca6330c5092119a403a569b6bb251e75539
imagiczhang <imagiczhang>
2006-07-24 02:29:48 +0000

    Batch script for installation package.

---
+++ start.sh
@@ -0,0 +1,21 @@
+# (c) Copyright IBM Corp. 2006 All Rights Reserved.
+
+#!/bin/bash
+
+# Get the absolute path of DITAOT's home directory
+DITA_DIR=$PWD
+
+export ANT_HOME=$DITA_DIR/tools/ant
+export PATH=$PATH:$DITA_DIR/tools/ant/bin
+export CLASSPATH=$CLASSPATH:$DITA_DIR/lib/dost.jar:$DITA_DIR/lib/fop.jar:$DITA_IDR/lib/avalon-framework-cvs-20020806.jar:$DITA_DIR/lib/batik.jar:$DITA_DIR/lib/xalan.jar:$DITA_DIR/lib/xercersImpl.jar:$DITA_DIR/lib/serializer.jar:$DITA_DIR/lib/xml-apis.jar
+
+ANT_CMD_LINE_ARGS=""
+while [ "$1" ]
+do
+	ANT_CMD_LINE_ARGS="$ANT_CMD_LINE_ARGS $1"
+	shift 1
+done
+
+ant $ANT_CMD_LINE_ARGS
+
+echo $ANT_CMD_LINE
