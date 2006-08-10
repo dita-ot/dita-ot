592bd898ce85abb85454a9ef7d002face6f562b8
imagiczhang <imagiczhang>
2006-08-10 02:33:43 +0000

    Add icu4j into the classpath. Add a statment that make ant executable Transform linux shell scirpts to Unix format

--- startcmd.bat
+++ startcmd.bat
@@ -4,5 +4,5 @@ set DITA_DIR=%~dp0
 REM Set environment variables
 set ANT_HOME=%DITA_DIR%tools\ant
 set PATH=%PATH%;%DITA_DIR%tools\ant\bin;
-set CLASSPATH=%CLASSPATH%;%DITA_DIR%lib\dost.jar;%DITA_DIR%lib\fop.jar;%DITA_DIR%lib\avalon-framework-cvs-20020806.jar;%DITA_DIR%lib\batik.jar;%DITA_DIR%lib\xalan.jar;%DITA_DIR%lib\xercesImpl.jar;%DITA_DIR%lib\serializer.jar;%DITA_DIR%lib\xml-apis.jar;
+set CLASSPATH=%CLASSPATH%;%DITA_DIR%lib\dost.jar;%DITA_DIR%lib\fop.jar;%DITA_DIR%lib\avalon-framework-cvs-20020806.jar;%DITA_DIR%lib\batik.jar;%DITA_DIR%lib\xalan.jar;%DITA_DIR%lib\xercesImpl.jar;%DITA_DIR%lib\serializer.jar;%DITA_DIR%lib\xml-apis.jar;%DITA_DIR%lib\icu4j.jar;
 start "DITA-OT" cmd.exe
