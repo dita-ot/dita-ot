7289e505ce7847bcca71ecf5fb45afeeb11a7ed6
imagiczhang <imagiczhang>
2006-08-11 09:38:45 +0000

    Correct a misspelling DITA_IDR Transform to UNIX format again

--- startcmd.sh
+++ startcmd.sh
@@ -8,6 +8,6 @@ chmod +x $DITA_DIR/tools/ant/bin/ant
 
 export ANT_HOME=$DITA_DIR/tools/ant
 export PATH=$PATH:$DITA_DIR/tools/ant/bin
-export CLASSPATH=$CLASSPATH:$DITA_DIR/lib/dost.jar:$DITA_DIR/lib/fop.jar:$DITA_IDR/lib/avalon-framework-cvs-20020806.jar:$DITA_DIR/lib/batik.jar:$DITA_DIR/lib/xalan.jar:$DITA_DIR/lib/xercersImpl.jar:$DITA_DIR/lib/serializer.jar:$DITA_DIR/lib/xml-apis.jar:$DITA_DIR/lib/icu4j.jar
+export CLASSPATH=$CLASSPATH:$DITA_DIR/lib/dost.jar:$DITA_DIR/lib/fop.jar:$DITA_DIR/lib/avalon-framework-cvs-20020806.jar:$DITA_DIR/lib/batik.jar:$DITA_DIR/lib/xalan.jar:$DITA_DIR/lib/xercersImpl.jar:$DITA_DIR/lib/serializer.jar:$DITA_DIR/lib/xml-apis.jar:$DITA_DIR/lib/icu4j.jar
 
 bash
