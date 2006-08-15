ac98cfc5d1e68203de5c310dfddc356041082a74
imagiczhang <imagiczhang>
2006-08-15 15:48:34 +0000

    add setting to include lib and lib/resolver.jar in CLASSPATH

--- startcmd.sh
+++ startcmd.sh
@@ -8,6 +8,6 @@ chmod +x $DITA_DIR/tools/ant/bin/ant
 
 export ANT_HOME=$DITA_DIR/tools/ant
 export PATH=$DITA_DIR/tools/ant/bin:$PATH
-export CLASSPATH=$DITA_DIR/lib/dost.jar:$DITA_DIR/lib/fop.jar:$DITA_DIR/lib/avalon-framework-cvs-20020806.jar:$DITA_DIR/lib/batik.jar:$DITA_DIR/lib/xalan.jar:$DITA_DIR/lib/xercersImpl.jar:$DITA_DIR/lib/serializer.jar:$DITA_DIR/lib/xml-apis.jar:$DITA_DIR/lib/icu4j.jar:$CLASSPATH
+export CLASSPATH=$DITA_DIR/lib:$DITA_DIR/lib/dost.jar:$DITA_DIR/lib/resolver.jar:$DITA_DIR/lib/fop.jar:$DITA_DIR/lib/avalon-framework-cvs-20020806.jar:$DITA_DIR/lib/batik.jar:$DITA_DIR/lib/xalan.jar:$DITA_DIR/lib/xercersImpl.jar:$DITA_DIR/lib/serializer.jar:$DITA_DIR/lib/xml-apis.jar:$DITA_DIR/lib/icu4j.jar:$CLASSPATH
 
 bash
