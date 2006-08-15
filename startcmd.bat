bc9babe6fc8b86e7327b80d3d11e9f09d3bd62b6
imagiczhang <imagiczhang>
2006-08-15 07:26:57 +0000

    Move the added PATH & CLASSPATH environment to the begeinning

--- startcmd.bat
+++ startcmd.bat
@@ -3,6 +3,6 @@ set DITA_DIR=%~dp0
 
 REM Set environment variables
 set ANT_HOME=%DITA_DIR%tools\ant
-set PATH=%PATH%;%DITA_DIR%tools\ant\bin;
-set CLASSPATH=%CLASSPATH%;%DITA_DIR%lib\dost.jar;%DITA_DIR%lib\fop.jar;%DITA_DIR%lib\avalon-framework-cvs-20020806.jar;%DITA_DIR%lib\batik.jar;%DITA_DIR%lib\xalan.jar;%DITA_DIR%lib\xercesImpl.jar;%DITA_DIR%lib\serializer.jar;%DITA_DIR%lib\xml-apis.jar;%DITA_DIR%lib\icu4j.jar;
+set PATH=%DITA_DIR%tools\ant\bin;%PATH%
+set CLASSPATH=%DITA_DIR%lib\dost.jar;%DITA_DIR%lib\fop.jar;%DITA_DIR%lib\avalon-framework-cvs-20020806.jar;%DITA_DIR%lib\batik.jar;%DITA_DIR%lib\xalan.jar;%DITA_DIR%lib\xercesImpl.jar;%DITA_DIR%lib\serializer.jar;%DITA_DIR%lib\xml-apis.jar;%DITA_DIR%lib\icu4j.jar;%CLASSPATH%
 start "DITA-OT" cmd.exe
