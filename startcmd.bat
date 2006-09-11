5c077811a6685de563f83cf93eca96dc185797db
imagiczhang <imagiczhang>
2006-09-11 02:18:13 +0000

    Update script to remove 'serializer.jar' from CLASSPATH

--- startcmd.bat
+++ startcmd.bat
@@ -4,5 +4,5 @@ set DITA_DIR=%~dp0
 REM Set environment variables
 set ANT_HOME=%DITA_DIR%tools\ant
 set PATH=%DITA_DIR%tools\ant\bin;%PATH%
-set CLASSPATH=%DITA_DIR%lib;%DITA_DIR%lib\dost.jar;%DITA_DIR%lib\resolver.jar;%DITA_DIR%lib\fop.jar;%DITA_DIR%lib\avalon-framework-cvs-20020806.jar;%DITA_DIR%lib\batik.jar;%DITA_DIR%lib\xalan.jar;%DITA_DIR%lib\xercesImpl.jar;%DITA_DIR%lib\serializer.jar;%DITA_DIR%lib\xml-apis.jar;%DITA_DIR%lib\icu4j.jar;%CLASSPATH%
+set CLASSPATH=%DITA_DIR%lib;%DITA_DIR%lib\dost.jar;%DITA_DIR%lib\resolver.jar;%DITA_DIR%lib\fop.jar;%DITA_DIR%lib\avalon-framework-cvs-20020806.jar;%DITA_DIR%lib\batik.jar;%DITA_DIR%lib\xalan.jar;%DITA_DIR%lib\xercesImpl.jar;%DITA_DIR%lib\xml-apis.jar;%DITA_DIR%lib\icu4j.jar;%CLASSPATH%
 start "DITA-OT" cmd.exe
