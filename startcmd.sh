9eb3a4413f0a10feec0410d89a975763c0e13d6e
imagiczhang <imagiczhang>
2006-09-11 09:07:08 +0000

    Update shell script to set ANT_OPTS to ensure a large enough JVM heap size

--- startcmd.sh
+++ startcmd.sh
@@ -6,6 +6,7 @@ DITA_DIR=$PWD
 
 chmod +x $DITA_DIR/tools/ant/bin/ant
 
+export ANT_OPTS=-Xmx512m $ANT_OPTS
 export ANT_HOME=$DITA_DIR/tools/ant
 export PATH=$DITA_DIR/tools/ant/bin:$PATH
 export CLASSPATH=$DITA_DIR/lib:$DITA_DIR/lib/dost.jar:$DITA_DIR/lib/resolver.jar:$DITA_DIR/lib/fop.jar:$DITA_DIR/lib/avalon-framework-cvs-20020806.jar:$DITA_DIR/lib/batik.jar:$DITA_DIR/lib/xalan.jar:$DITA_DIR/lib/xercersImpl.jar:$DITA_DIR/lib/xml-apis.jar:$DITA_DIR/lib/icu4j.jar:$CLASSPATH
