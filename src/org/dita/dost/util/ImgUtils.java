83a1b8aa1f2fdb887d43d25bfbe202f9073facda
imagiczhang <imagiczhang>
2006-09-12 09:54:49 +0000

    Update copyright declaration

--- src/org/dita/dost/util/ImgUtils.java
+++ src/org/dita/dost/util/ImgUtils.java
@@ -1,3 +1,9 @@
+/*
+ * This file is part of the DITA Open Toolkit project hosted on
+ * Sourceforge.net. See the accompanying license.txt file for 
+ * applicable licenses.
+ */
+
 /**
  * (c) Copyright IBM Corp. 2006 All Rights Reserved.
  */
