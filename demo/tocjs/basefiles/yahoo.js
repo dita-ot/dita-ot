9d8e88bc6f968b3ef8b48ea11a93db64b9c69e2d
smckenzie23 <smckenzie23>
2007-05-23 22:35:06 +0000

    updated copyright notices

--- demo/tocjs/basefiles/yahoo.js
+++ demo/tocjs/basefiles/yahoo.js
@@ -1,4 +1,8 @@
-/*                                                                                                                                                      
+/*
+This file is part of the DITA Open Toolkit project hosted on
+Sourceforge.net. See the accompanying license.txt file for
+applicable licenses.                                                                                                                                                        
+                                                                                                                                                      
 Copyright (c) 2006, Yahoo! Inc. All rights reserved.                                                                                                    
 Code licensed under the BSD License:                                                                                                                    
 http://developer.yahoo.net/yui/license.txt                                                                                                              
