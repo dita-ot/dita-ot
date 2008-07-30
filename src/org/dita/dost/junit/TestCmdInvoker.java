6d0f647a1f1936703cff08face2835a21614986f
imagiczhang <imagiczhang>
2008-07-30 00:30:15 +0000

    new junit for commandlineInvoker

---
+++ src/org/dita/dost/junit/TestCmdInvoker.java
@@ -0,0 +1,30 @@
+package org.dita.dost.junit;
+
+import org.dita.dost.exception.DITAOTException;
+import org.dita.dost.invoker.CommandLineInvoker;
+import org.junit.Test;
+
+public class TestCmdInvoker {
+
+	@Test(expected = DITAOTException.class)
+	public void testProcessArguments() throws Exception {
+		String input[]={"/i:abc.ditamap","/transtypexhtml"};
+		CommandLineInvoker test = new CommandLineInvoker();
+		test.processArguments(input);
+	}
+	
+	@Test(expected = DITAOTException.class)
+	public void testProcessArgsWrongParam() throws Exception {
+		String input[]={"/i:abc.ditamap","/abc:def"};
+		CommandLineInvoker test = new CommandLineInvoker();		
+		test.processArguments(input);
+	}
+	
+	@Test(expected = DITAOTException.class)
+	public void testProcessArgsEmptyValue() throws Exception {
+		String input[]={"/i:"};
+		CommandLineInvoker test = new CommandLineInvoker();
+		test.processArguments(input);
+	}
+
+}
