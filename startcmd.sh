bcc9dcfe24058e817d54649e63f9c6e9fd0ee3d9
imagiczhang <imagiczhang>
2008-12-15 09:03:29 +0000

    FIX: The old fop plugin has been moved to demo/legacypdf/lib, necessary changes were made in build_init.xml and build_template in order to separate them thoroughly and redirect transtype pdf to pdf2. Removed the old fop.jar from the CLASSPATH in startcmd.[sh|bat]. Now classpath goes in their own build scripts.

--- startcmd.sh
+++ startcmd.sh
@@ -22,7 +22,7 @@ export ANT_OPTS="-Xmx512m $ANT_OPTS"
 export ANT_HOME="$DITA_DIR"/tools/ant
 export PATH="$DITA_DIR"/tools/ant/bin:"$PATH"
 
-NEW_CLASSPATH="$DITA_DIR/lib:$DITA_DIR/lib/dost.jar:$DITA_DIR/lib/resolver.jar:$DITA_DIR/lib/fop.jar:$DITA_DIR/lib/avalon-framework-cvs-20020806.jar:$DITA_DIR/lib/batik.jar:$DITA_DIR/lib/xalan.jar:$DITA_DIR/lib/xercersImpl.jar:$DITA_DIR/lib/xml-apis.jar:$DITA_DIR/lib/icu4j.jar"
+NEW_CLASSPATH="$DITA_DIR/lib:$DITA_DIR/lib/dost.jar:$DITA_DIR/lib/resolver.jar:$DITA_DIR/lib/avalon-framework-cvs-20020806.jar:$DITA_DIR/lib/batik.jar:$DITA_DIR/lib/xalan.jar:$DITA_DIR/lib/xercersImpl.jar:$DITA_DIR/lib/xml-apis.jar:$DITA_DIR/lib/icu4j.jar"
 if test -n "$CLASSPATH"
 then
 export CLASSPATH="$NEW_CLASSPATH":"$CLASSPATH"
