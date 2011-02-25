2283b78ef6d062f58a4ddcf039bcec74cbce31e0
Jarno Elovirta <jarno@elovirta.com>
2011-02-25 13:35:47 +0200

    Update and clean extensible Ant pipeline task - ID: 3158929

--- src/org/dita/dost/invoker/ExtensibleAntInvoker.java
+++ src/org/dita/dost/invoker/ExtensibleAntInvoker.java
@@ -7,101 +7,140 @@
 /*
  * (c) Copyright IBM Corp. 2004, 2008 All Rights Reserved.
  */
-
 package org.dita.dost.invoker;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Properties;
 import java.util.StringTokenizer;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
+
 import org.dita.dost.exception.DITAOTException;
-import org.dita.dost.log.DITAOTJavaLogger;
+import org.dita.dost.log.DITAOTAntLogger;
+import org.dita.dost.log.DITAOTLogger;
 import org.dita.dost.log.MessageUtils;
 import org.dita.dost.pipeline.PipelineFacade;
 import org.dita.dost.pipeline.PipelineHashIO;
 import org.dita.dost.util.StringUtils;
+
 /**
  * Extensible Ant Invoker.
- *
+ * 
+ * @author Deborah Pickett
  */
-public class ExtensibleAntInvoker extends Task {
-	/**key value pair separator.*/
+public final class ExtensibleAntInvoker extends Task {
+    
+	/** Key value pair separator. */
 	private final static String KEY_VALUE_PAIR_SEPARATOR = ";";
-	/**equal sign.*/
+	/** Equal sign. */
 	private final static String KEY_VALUE_EQUAL_SIGN = "=";
-	/**logger.*/
-	private DITAOTJavaLogger javaLogger = null;
-	/**pipeline.*/
-	private PipelineFacade pipeline;
-	/**hashIO.*/
-	private PipelineHashIO pipelineInput;
-	/**params.*/
-	private ArrayList<Param> params;
+	
+	/** Logger. */
+	private DITAOTLogger logger;
+	/** Pipeline. */
+	private final PipelineFacade pipeline;
+	/** Pipeline input. */
+	private final PipelineHashIO pipelineInput;
+	/** Nested params. */
+	private final ArrayList<Param> params;
+	
 	/**
 	 * Constructor.
 	 */
 	public ExtensibleAntInvoker() {
 		super();
-		javaLogger = new DITAOTJavaLogger();
 		pipeline = new PipelineFacade();
 		pipelineInput = new PipelineHashIO();
 		params = new ArrayList<Param>();
 	}
+	
 	/**
-	 * set base dir.
-	 * @param s string
+	 * Set base directory.
+	 * @param s base directory
 	 */
 	public void setBasedir(String s) {
 		pipelineInput.setAttribute("basedir", s);
 	}
+	
 	/**
-	 * get Base dir.
-	 * @return baseDir
+	 * Get base directory
+	 * @return base directory
 	 */
 	public String getBasedir() {
 		return pipelineInput.getAttribute("basedir");
 	}
+	
 	/**
-	 * set Module.
-	 * @param module module
+	 * Set module.
+	 * @param module module name
 	 */
 	public void setModule(String module) {
 		pipelineInput.setAttribute("module", module);
 	}
+	
 	/**
-	 * get module.
-	 * @return module
+	 * Get module.
+	 * @return module name
 	 */
 	public String getModule() {
 		return pipelineInput.getAttribute("module");
 	}
+	
 	/**
-	 * set message.
+	 * Set message.
 	 * @param m message
 	 */
 	public void setMessage(String m) {
 		pipelineInput.setAttribute("message", m);
 	}
+	
 	/**
-	 * get message.
+	 * Get message.
 	 * @return message
 	 */
 	public String getMessage() {
 		return pipelineInput.getAttribute("message");
 	}
 	
+    /**
+     * Set input data.
+     * @param input data        
+     */
+    public void setInputdita(String inputdita) {
+        pipelineInput.setAttribute("inputdita", inputdita);
+    }
+
+    /**
+     * Set input map.
+     * @param input map           
+     */
+    public void setInputmap(String inputmap) {
+        pipelineInput.setAttribute("inputmap", inputmap);
+    }
+    
+    /**
+     * Set temporary directory.
+     * @param tempdir temporary directory
+     */
+    public void setTempdir(String tempdir) {
+        pipelineInput.setAttribute("tempDir", tempdir);
+    }
+	
 	/**
 	 * Set extra parameter values for input.
-	 * @param extParam extended parameters string, key value pair string separated by
-	 *            ";" eg. extparam="maplinks=XXXX;other=YYYY"          
+	 * 
+	 * Value is a key-value pair string separated by ";" e.g.
+	 * {@code maplinks=XXXX;other=YYYY}
+	 * 
+	 * @param extParam extended parameters string          
 	 */
 	public void setExtparam(String extParam) {
 		String keyValueStr = null;
 		String attrName = null;
 		String attrValue = null;
-		StringTokenizer extParamStrTokenizer = new StringTokenizer(extParam,
+		final StringTokenizer extParamStrTokenizer = new StringTokenizer(extParam,
 				KEY_VALUE_PAIR_SEPARATOR);
 
 		while (extParamStrTokenizer.hasMoreTokens()) {
@@ -111,7 +150,7 @@ public class ExtensibleAntInvoker extends Task {
 
 			if (p <= 0) {
 				String msg = null;
-				Properties params = new Properties();
+				final Properties params = new Properties();
 
 				params.put("%1", keyValueStr);
 				msg = MessageUtils.getMessage("DOTJ006F", params).toString();
@@ -124,7 +163,7 @@ public class ExtensibleAntInvoker extends Task {
 			if (StringUtils.isEmptyString(attrName) ||
 					StringUtils.isEmptyString(attrValue)) {
 				String msg = null;
-				Properties params = new Properties();
+				final Properties params = new Properties();
 
 				params.put("%1", keyValueStr);
 				msg = MessageUtils.getMessage("DOTJ006F", params).toString();
@@ -137,102 +176,146 @@ public class ExtensibleAntInvoker extends Task {
 	}
 
 	/** 
-	 * Handle nested parameters.  Add the key/value to the pipeline hash
-	 * only if the "if" attribute refers to a property that exists.
-	 * @return param
+	 * Handle nested parameters. Add the key/value to the pipeline hash, unless
+	 * the "if" attribute is set and refers to a unset property.
+	 * @return parameter
 	 */
 	public Param createParam() {
-		Param p = new Param();
+		final Param p = new Param();
 		params.add(p);
 		return p;
 	}
 
 	/**
-	 * execution point of this invoker.
+	 * Execution point of this invoker.
 	 * @throws BuildException exception
 	 */
 	public void execute() throws BuildException {
+	    logger = new DITAOTAntLogger(getProject());
 		if (getModule() == null) {
-			throw new BuildException("module attribute must be specified");
+			throw new BuildException("Module attribute must be specified");
 		}
 		/* Set basedir if not already set. */
 		if (pipelineInput.getAttribute("basedir") == null) {
 			pipelineInput.setAttribute("basedir", getProject().getBaseDir().getAbsolutePath());
 		}
 		/* Set params. */
-		for (Param p : params) {
+		for (final Param p : params) {
 			if (!p.isValid()) {
 				throw new BuildException("Incomplete parameter");
 			}
 			// Check the "if" attribute.
-			String ifProperty = p.getIf();
-			if (ifProperty == null
-					|| getProject().getProperties().containsKey(ifProperty))
-			{
-				pipelineInput.setAttribute(p.getName(), p.getExpression());
+			final String ifProperty = p.getIf();
+			final String unlessProperty = p.getUnless();
+			if ((ifProperty == null || getProject().getProperties().containsKey(ifProperty))
+			        && (unlessProperty == null || !getProject().getProperties().containsKey(unlessProperty))) {
+				pipelineInput.setAttribute(p.getName(), p.getValue());
 			}
 		}
 
 		try {
 			pipeline.execute(getModule(), pipelineInput);
-		} catch (DITAOTException e) {
-			throw new BuildException(e.getMessage(), e);
+		} catch (final DITAOTException e) {
+			throw new BuildException("Failed to run pipeline: " + e.getMessage(), e);
 		}
 	}
 	
 	/** Nested parameters. */
 	public static class Param {
+	    
 		private String name;
 		private String value;
-		private String ifproperty;
+		private String ifProperty;
+		private String unlessProperty;
+		
 		/**
-		 * get name.
-		 * @return name
+		 * Get parameter name.
+		 * @return parameter name, {@code null} if not set
 		 */
 		public String getName() {
 			return name;
 		}
+		
 		/**
-		 * 
-		 * @return isValid
+		 * Validate that all required attributes have been set.
+		 * @return isValid {@code true} is valid object, otherwise {@code false}
 		 */
 		public boolean isValid() {
 			return (name != null && value != null);
 		}
+		
 		/**
-		 * set name.
+		 * Set parameter name.
 		 * @param s name
 		 */
 		public void setName(String s) {
 			name = s;
 		}
+		
 		/**
-		 * get Exception.
-		 * @return exception
+		 * Get parameter value.
+		 * @return parameter value, {@code null} if not set
 		 */
-		public String getExpression() {
+		public String getValue() {
 			return value;
 		}
+		
 		/**
-		 * 
-		 * @param v expression
+		 * Set parameter value.
+		 * @param v parameter value
 		 */
 		public void setExpression(String v) {
 			value = v;
 		}
+		
 		/**
-		 * 
-		 * @return if
+         * Set parameter value.
+         * @param v parameter value
+         */
+        public void setValue(String v) {
+            value = v;
+        }
+        
+        /**
+         * Set parameter file value.
+         * @param v parameter file value
+         */
+        public void setLocation(File v) {
+            value = v.getPath();
+        }
+		
+		/**
+		 * Get if condition property name
+		 * @return if condition parameter name, {@code null} if not set
 		 */
 		public String getIf() {
-			return ifproperty;
+			return ifProperty;
 		}
+		
 		/**
-		 * 
-		 * @param p if
+		 * Set if condition parameter name
+		 * @param p parameter name
 		 */
 		public void setIf(String p) {
-			ifproperty = p;
+			ifProperty = p;
 		}
+	
+		/**
+         * Get unless condition property name
+         * @return unless condition parameter name, {@code null} if not set
+         */
+        public String getUnless() {
+            return unlessProperty;
+        }
+        
+        /**
+         * Set unless condition parameter name
+         * @param p parameter name
+         */
+        public void setUnless(String p) {
+            unlessProperty = p;
+        }
+		
 	}
+
 }
