4dab09604b76e5dbde163987bb2c262eb334db49
Jarno Elovirta <jarno@elovirta.com>
2011-03-18 10:32:14 +0200

    Add MergeTopicParser and MergeMapParser unit tests - ID: 3160801
    Add XMLUnit to allow better XML result comparison.

---
+++ test/java/org/dita/dost/reader/MergeMapParserTest.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the DITA Open Toolkit project hosted on
+ * Sourceforge.net. See the accompanying license.txt file for 
+ * applicable licenses.
+ */
+
+/*
+ * (c) Copyright IBM Corp. 2011 All Rights Reserved.
+ */
+package org.dita.dost.reader;
+
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.xml.sax.SAXException;
+
+import org.xml.sax.InputSource;
+
+import org.dita.dost.TestUtils;
+
+import org.junit.Test;
+
+public class MergeMapParserTest {
+
+    private final File resourceDir = new File("test-stub", MergeMapParserTest.class.getSimpleName());
+    private final File srcDir = new File(resourceDir, "src");
+    private final File expDir = new File(resourceDir, "exp");
+    
+    @Test
+    public void testRead() throws SAXException, IOException {
+        final MergeMapParser parser = new MergeMapParser();
+        parser.setLogger(new TestUtils.TestLogger());
+        parser.read(new File(srcDir, "test.ditamap").getAbsolutePath() + "|" + srcDir.getAbsolutePath());
+        final String act = "<wrapper>" + parser.getContent().getValue().toString() + "</wrapper>";
+        assertXMLEqual(new InputSource(new File(expDir, "merged.xml").toURI().toString()),
+                       new InputSource(new StringReader(act)));
+    }
+
+}
