282f80140d21cb9720c4b8a398c28f73887a1388
Jason <shjle@cn.ibm.com>
2011-03-29 14:35:51 +0800

    Set Java logger for ExtensibleAntInvoker

--- src/org/dita/dost/invoker/ExtensibleAntInvoker.java
+++ src/org/dita/dost/invoker/ExtensibleAntInvoker.java
@@ -17,7 +17,7 @@ import java.util.StringTokenizer;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 import org.dita.dost.exception.DITAOTException;
-import org.dita.dost.log.DITAOTAntLogger;
+import org.dita.dost.log.DITAOTJavaLogger;
 import org.dita.dost.log.MessageUtils;
 import org.dita.dost.pipeline.PipelineFacade;
 import org.dita.dost.pipeline.PipelineHashIO;
@@ -208,7 +208,7 @@ public final class ExtensibleAntInvoker extends Task {
 			}
 		}
 
-		pipeline.setLogger(new DITAOTAntLogger(getProject()));
+		pipeline.setLogger(new DITAOTJavaLogger());
 		try {
 			pipeline.execute(getModule(), pipelineInput);
 		} catch (final DITAOTException e) {
