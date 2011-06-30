3bb57efeed957fcfb28d10ebe60abbfdf9adc8da
Jarno Elovirta <jarno@elovirta.com>
2011-06-30 12:36:54 +0300

    Fix HTMLHelp index writer unit test - ID: 3346094
    Add validator.nu to compare HTML documents.

--- test/java/org/dita/dost/writer/TestCHMIndexWriter.java
+++ test/java/org/dita/dost/writer/TestCHMIndexWriter.java
@@ -8,48 +8,103 @@
  * (c) Copyright IBM Corp. 2010 All Rights Reserved.
  */
 package org.dita.dost.writer;
-import static org.junit.Assert.assertEquals;
+
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import nu.validator.htmlparser.dom.HtmlDocumentBuilder;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import org.dita.dost.TestUtils;
-import org.dita.dost.module.ContentImpl;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.dita.dost.exception.DITAOTException;
-import org.dita.dost.writer.CHMIndexWriter;
-public class TestCHMIndexWriter {
-	
-	private File tempDir;
-	
-	public static CHMIndexWriter chmindexwriter = new CHMIndexWriter();
-	public static ContentImpl content = new ContentImpl();
-	
-	@Before
-	public void setUp() throws IOException, DITAOTException {
-		tempDir = TestUtils.createTempDir(getClass());
-	}
-	
-	@Test
-	public void testgetIndexFileName(){
-		
-		
-		assertEquals(new File(tempDir, "a.xml.hhk").getAbsolutePath(),
-				     (chmindexwriter.getIndexFileName(new File(tempDir,"a.xml").getAbsolutePath())));
+import org.dita.dost.index.IndexTerm;
+import org.dita.dost.module.Content;
+import org.dita.dost.module.ContentImpl;
 
-	}
-	
-	@Test(expected = DITAOTException.class)
-	public void testwrite() throws DITAOTException
-	{
-		String filename = new File(tempDir, "a.xml").getAbsolutePath();
-		chmindexwriter.write(filename);
-	}
-	
-	@After
-	public void tearDown() throws IOException {
-		TestUtils.forceDelete(tempDir);
-	}
+public class TestCHMIndexWriter {
+
+    private static File tempDir;
+    private static final File resourceDir = new File("test-stub", TestCHMIndexWriter.class.getSimpleName());
+    private static final File expDir = new File(resourceDir, "exp");
+
+    @BeforeClass
+    public static void setUp() throws IOException, DITAOTException {
+        tempDir = TestUtils.createTempDir(TestCHMIndexWriter.class);
+        final DocumentBuilderFactory factory = new HTMLDocumentBuilderFactory();
+        XMLUnit.setControlDocumentBuilderFactory(factory);
+        XMLUnit.setTestDocumentBuilderFactory(factory);
+        XMLUnit.setIgnoreWhitespace(true);
+        XMLUnit.setIgnoreComments(true);
+    }
+
+    @Test
+    public void testWrite() throws DITAOTException, SAXException, IOException {
+        final Content content = new ContentImpl();
+        final IndexTerm indexterm1 = new IndexTerm();
+        indexterm1.setTermName("name1");
+        indexterm1.setTermKey("indexkey1");
+        final IndexTerm indexterm2 = new IndexTerm();
+        indexterm2.setTermName("name2");
+        indexterm2.setTermKey("indexkey2");
+        indexterm1.addSubTerm(indexterm2);
+        final Collection<IndexTerm> collection = new ArrayList<IndexTerm>();
+        collection.add(indexterm1);
+        content.setCollection(collection);
+        
+        final CHMIndexWriter indexWriter = new CHMIndexWriter();
+        indexWriter.setContent(content);
+        final File outFile = new File(tempDir, "index.hhk");
+        indexWriter.write(outFile.getAbsolutePath());
+        
+        assertXMLEqual(new InputSource(new File(expDir, "index.hhk").toURI().toString()),
+                       new InputSource(outFile.toURI().toString()));
+    }
+
+    @AfterClass
+    public static void tearDown() throws IOException {
+        TestUtils.forceDelete(tempDir);
+    }
+    
+    private static class HTMLDocumentBuilderFactory extends DocumentBuilderFactory {
+        
+        @Override
+        public Object getAttribute(String arg0) throws IllegalArgumentException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean getFeature(String arg0) throws ParserConfigurationException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public DocumentBuilder newDocumentBuilder() throws ParserConfigurationException {
+            return new HtmlDocumentBuilder();
+        }
+
+        @Override
+        public void setAttribute(String arg0, Object arg1) throws IllegalArgumentException {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void setFeature(String arg0, boolean arg1) throws ParserConfigurationException {
+            throw new UnsupportedOperationException();           
+        }
+        
+    }
 
 }
