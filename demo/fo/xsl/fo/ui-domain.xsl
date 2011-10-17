dita-ot/dita-ot

commit 6818459119a771463a20c88a0abc72b9a0a31dd3

Remove whitespace from menucascade - ID: 3423537
The preferred way would be to use xsl:strip-space to remove whitespace
from menucascade during tree build time, but it will not support
specialization. The next way is to not select menucascade for output,
option taken by the PDF processing. XHTML uses flag processing and makes
not choosing the text nodes difficult, thus it takes the approach of
having a template for menucascade text nodes and ignores them that way.

demo/fo/xsl/fo/ui-domain.xsl
@@ -38,9 +38,7 @@ See the accompanying license.txt file for applicable licenses.
     <xsl:template match="*[contains(@class,' ui-d/uicontrol ')]">
         <!-- insert an arrow before all but the first uicontrol in a menucascade -->
         <xsl:if test="ancestor::*[contains(@class,' ui-d/menucascade ')]">
-            <xsl:variable name="uicontrolcount">
-                <xsl:number count="*[contains(@class,' ui-d/uicontrol ')]"/>
-            </xsl:variable>
+            <xsl:variable name="uicontrolcount" select="preceding-sibling::*[contains(@class,' ui-d/uicontrol ')]"/>
             <xsl:if test="$uicontrolcount &gt; 1">
                 <xsl:text> &gt; </xsl:text>
             </xsl:if>
@@ -61,7 +59,7 @@ See the accompanying license.txt file for applicable licenses.
     <xsl:template match="*[contains(@class,' ui-d/menucascade ')]">
         <fo:inline xsl:use-attribute-sets="menucascade">
             <xsl:call-template name="commonattributes"/>
-            <xsl:apply-templates/>
+            <xsl:apply-templates select="* | processing-instruction() | comment()"/>
         </fo:inline>
     </xsl:template>
 

xsl/xslhtml/ui-d.xsl
@@ -110,6 +110,8 @@
    </xsl:call-template>
   </span>
 </xsl:template>
+<!-- Ignore text inside menucascade -->
+<xsl:template match="*[contains(@class,' ui-d/menucascade ')]/text()"/>
 
 <xsl:template match="*[contains(@class,' ui-d/shortcut ')]" name="topic.ui-d.shortcut">
   <xsl:variable name="flagrules">

commit a45718f0ff9b634e0e9f2d8ab39cc9648827e1f6

Fix menucascade numbering bug.
Bug introduced in commit 6818459119a771463a20c88a0abc72b9a0a31dd3.

demo/fo/xsl/fo/ui-domain.xsl
@@ -38,8 +38,8 @@ See the accompanying license.txt file for applicable licenses.
     <xsl:template match="*[contains(@class,' ui-d/uicontrol ')]">
         <!-- insert an arrow before all but the first uicontrol in a menucascade -->
         <xsl:if test="ancestor::*[contains(@class,' ui-d/menucascade ')]">
-            <xsl:variable name="uicontrolcount" select="preceding-sibling::*[contains(@class,' ui-d/uicontrol ')]"/>
-            <xsl:if test="$uicontrolcount &gt; 1">
+            <xsl:variable name="uicontrolcount" select="count(preceding-sibling::*[contains(@class,' ui-d/uicontrol ')])"/>
+            <xsl:if test="$uicontrolcount &gt; 0">
                 <xsl:text> &gt; </xsl:text>
             </xsl:if>
         </xsl:if>