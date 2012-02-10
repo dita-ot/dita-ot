b55fd8054861e47c078968ae70cab8b23ae5799a
Jarno Elovirta <jarno@elovirta.com>
2012-02-10 11:52:35 +0200

    Add args.filter argument - ID: 3023241
    Deprecate dita.input.valfile as an argument property.

--- src/org/dita/dost/log/DITAOTFailTask.java
+++ src/org/dita/dost/log/DITAOTFailTask.java
@@ -9,10 +9,15 @@
  */
 package org.dita.dost.log;
 
+import static org.dita.dost.log.MessageBean.*;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Properties;
 import java.util.StringTokenizer;
+
+import org.apache.tools.ant.taskdefs.condition.Condition;
+import org.apache.tools.ant.taskdefs.condition.ConditionBase;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.taskdefs.Exit;
@@ -20,9 +25,7 @@ import org.dita.dost.exception.DITAOTException;
 import org.dita.dost.invoker.ExtensibleAntInvoker.Param;
 
 /**
- * Ant fail task for custom error message.
- *
- * @author Wu, Zhi Qiang
+ * Ant echo task for custom error message.
  */
 public final class DITAOTFailTask extends Exit {
     private String id = null;
@@ -80,6 +83,13 @@ public final class DITAOTFailTask extends Exit {
      */
     @Override
     public void execute() throws BuildException {
+        boolean fail = nestedConditionPresent()
+                       ? testNestedCondition()
+                       : (testIfCondition() && testUnlessCondition());
+        if (!fail) {
+            return;
+        }
+        
         if (id == null) {
             throw new BuildException("id attribute must be specified");
         }
@@ -96,13 +106,29 @@ public final class DITAOTFailTask extends Exit {
         }
         
         initMessageFile();
-        final MessageBean msgBean=MessageUtils.getMessage(id, prop);
-        setMessage(msgBean.toString());
-        try{
-            super.execute();
-        }catch(final BuildException ex){
-            throw new BuildException(msgBean.toString(),new DITAOTException(msgBean,ex,msgBean.toString()));
+        final MessageBean msgBean = MessageUtils.getMessage(id, prop);
+        final DITAOTLogger logger = new DITAOTAntLogger(getProject());
+        if (msgBean != null) {
+            final String type = msgBean.getType();
+            if(FATAL.equals(type)){
+                setMessage(msgBean.toString());
+                try{
+                    super.execute();
+                }catch(final BuildException ex){
+                    throw new BuildException(msgBean.toString(),new DITAOTException(msgBean,ex,msgBean.toString()));
+                }
+            } else if(ERROR.equals(type)){
+                logger.logError(msgBean.toString());
+            } else if(WARN.equals(type)){
+                logger.logWarn(msgBean.toString());
+            } else if(INFO.equals(type)){
+                logger.logInfo(msgBean.toString());
+            } else if(DEBUG.equals(type)){
+                logger.logDebug(msgBean.toString());
+            }
         }
+        
+        
     }
 
     private void initMessageFile() {
@@ -120,6 +146,127 @@ public final class DITAOTFailTask extends Exit {
         }
 
         MessageUtils.loadMessages(messageFile);
+    }
+    
+    // Ant Exit class methods --------------------------------------------------
+    
+    private static class NestedCondition extends ConditionBase implements Condition {
+        public boolean eval() {
+            if (countConditions() != 1) {
+                throw new BuildException(
+                    "A single nested condition is required.");
+            }
+            return ((Condition) (getConditions().nextElement())).eval();
+        }
+    }
+
+    private String message;
+    private String ifCondition, unlessCondition;
+    private NestedCondition nestedCondition;
+    private Integer status;
+
+    /**
+     * A message giving further information on why the build exited.
+     *
+     * @param value message to output
+     */
+    public void setMessage(String value) {
+        this.message = value;
+    }
+
+    /**
+     * Only fail if a property of the given name exists in the current project.
+     * @param c property name
+     */
+    public void setIf(String c) {
+        ifCondition = c;
+    }
+
+    /**
+     * Only fail if a property of the given name does not
+     * exist in the current project.
+     * @param c property name
+     */
+    public void setUnless(String c) {
+        unlessCondition = c;
+    }
+
+    /**
+     * Set the status code to associate with the thrown Exception.
+     * @param i   the <code>int</code> status
+     */
+    public void setStatus(int i) {
+        status = new Integer(i);
+    }
+
+    /**
+     * Set a multiline message.
+     * @param msg the message to display
+     */
+    public void addText(String msg) {
+        if (message == null) {
+            message = "";
+        }
+        message += getProject().replaceProperties(msg);
+    }
+
+    /**
+     * Add a condition element.
+     * @return <code>ConditionBase</code>.
+     * @since Ant 1.6.2
+     */
+    public ConditionBase createCondition() {
+        if (nestedCondition != null) {
+            throw new BuildException("Only one nested condition is allowed.");
+        }
+        nestedCondition = new NestedCondition();
+        return nestedCondition;
+    }
+
+    /**
+     * test the if condition
+     * @return true if there is no if condition, or the named property exists
+     */
+    private boolean testIfCondition() {
+        if (ifCondition == null || "".equals(ifCondition)) {
+            return true;
+        }
+        return getProject().getProperty(ifCondition) != null;
+    }
+
+    /**
+     * test the unless condition
+     * @return true if there is no unless condition,
+     *  or there is a named property but it doesn't exist
+     */
+    private boolean testUnlessCondition() {
+        if (unlessCondition == null || "".equals(unlessCondition)) {
+            return true;
+        }
+        return getProject().getProperty(unlessCondition) == null;
+    }
+
+    /**
+     * test the nested condition
+     * @return true if there is none, or it evaluates to true
+     */
+    private boolean testNestedCondition() {
+        boolean result = nestedConditionPresent();
+
+        if (result && ifCondition != null || unlessCondition != null) {
+            throw new BuildException("Nested conditions "
+                + "not permitted in conjunction with if/unless attributes");
+        }
+
+        return result && nestedCondition.eval();
+    }
+
+    /**
+     * test whether there is a nested condition.
+     * @return <code>boolean</code>.
+     */
+    private boolean nestedConditionPresent() {
+        return (nestedCondition != null);
     }
 
 }
