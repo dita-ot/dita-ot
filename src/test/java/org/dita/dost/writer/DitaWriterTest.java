0c7a893db7058301ff73c661ae98c2e17a6eb684
Jarno Elovirta <jarno@elovirta.com>
2012-05-28 16:16:04 +0300

    Improve DitaWriter unit test #1072

--- src/test/java/org/dita/dost/writer/DitaWriterTest.java
+++ src/test/java/org/dita/dost/writer/DitaWriterTest.java
@@ -16,6 +16,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -24,6 +26,8 @@ import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.xml.sax.helpers.AttributesImpl;
 
 import org.w3c.dom.NodeList;
 
@@ -51,6 +55,7 @@ import org.dita.dost.util.DelayConrefUtils;
 import org.dita.dost.util.FileUtils;
 import org.dita.dost.util.FilterUtils;
 import org.dita.dost.util.OutputUtils;
+import org.dita.dost.util.XMLUtils;
 
 public class DitaWriterTest {
 
@@ -156,7 +161,64 @@ public class DitaWriterTest {
         }
         return d;
     }
-
+    
+    @Test
+    public void testReplaceHref() throws Exception {
+        final Invoker w = new Invoker("replaceHREF", ATTRIBUTE_NAME_HREF, String.class, Attributes.class) {
+            @Override
+            public String invoke(final String value) throws Exception {
+                final AttributesImpl atts = new AttributesImpl();
+                XMLUtils.addOrSetAttribute(atts, ATTRIBUTE_NAME_CLASS, TOPIC_XREF.toString());
+                XMLUtils.addOrSetAttribute(atts, ATTRIBUTE_NAME_SCOPE, ATTR_SCOPE_VALUE_LOCAL);
+                XMLUtils.addOrSetAttribute(atts, ATTRIBUTE_NAME_FORMAT, ATTR_FORMAT_VALUE_DITA);
+                XMLUtils.addOrSetAttribute(atts, attrName, value);
+                return (String) method.invoke(writer, attrName, atts);    
+            }
+        };
+        // same directory path
+        assertEquals("foo +%25bar.dita", w.invoke("foo +%25bar.dita"));
+        assertEquals("foo.dita#bar", w.invoke("foo.dita#bar"));
+        // absolute same directory path
+        assertEquals("foo.dita", w.invoke(new File(srcDir, "foo.dita").getAbsolutePath()));
+        assertEquals("foo.dita#bar", w.invoke(new File(srcDir, "foo.dita").getAbsolutePath() + "#bar"));
+        final File sub = new File(srcDir, "sub" + File.separator + "foo +%bar.dita").getAbsoluteFile();
+        // absolute sub directory path
+        assertEquals("sub/foo +%bar.dita", w.invoke(sub.getAbsolutePath()));
+        assertEquals("sub/foo +%bar.dita#bar", w.invoke(sub.getAbsolutePath() + "#bar"));
+        // absolute sub directory URI
+        assertEquals("sub/foo%20+%25bar.dita", w.invoke(sub.toURI().toASCIIString()));
+        assertEquals("sub/foo%20+%25bar.dita#bar", w.invoke(sub.toURI().toASCIIString() + "#bar"));
+        // unsupported extension
+        assertEquals("foo.dita", w.invoke("foo.bar"));
+    }
+    
+    @Test
+    public void testReplaceConref() throws Exception {
+        final Invoker w = new Invoker("replaceCONREF", ATTRIBUTE_NAME_CONREF, Attributes.class) {
+            @Override
+            public String invoke(final String value) throws Exception {
+                final AttributesImpl atts = new AttributesImpl();
+                XMLUtils.addOrSetAttribute(atts, attrName, value);
+                return (String) method.invoke(writer, atts);    
+            }
+        };
+        // same directory path
+        assertEquals("foo +%25bar.dita", w.invoke("foo +%25bar.dita"));
+        assertEquals("foo.dita#bar", w.invoke("foo.dita#bar"));
+        // absolute same directory path
+        assertEquals("foo.dita", w.invoke(new File(srcDir, "foo.dita").getAbsolutePath()));
+        assertEquals("foo.dita#bar", w.invoke(new File(srcDir, "foo.dita").getAbsolutePath() + "#bar"));
+        final File sub = new File(srcDir, "sub" + File.separator + "foo +%bar.dita").getAbsoluteFile();
+        // absolute sub directory path
+        assertEquals("sub/foo +%bar.dita", w.invoke(sub.getAbsolutePath()));
+        assertEquals("sub/foo +%bar.dita#bar", w.invoke(sub.getAbsolutePath() + "#bar"));
+        // absolute sub directory URI
+        assertEquals(srcDir.toURI().toASCIIString() + "sub/foo%20+%25bar.dita", w.invoke(sub.toURI().toASCIIString()));
+        assertEquals(srcDir.toURI().toASCIIString() + "sub/foo%20+%25bar.dita#bar", w.invoke(sub.toURI().toASCIIString() + "#bar"));
+        // unsupported extension
+        assertEquals("foo.dita", w.invoke("foo.bar"));
+    }
+    
     @AfterClass
     public static void tearDown() throws IOException {
         TestUtils.forceDelete(tempDir);
@@ -238,4 +300,22 @@ public class DitaWriterTest {
         }
 
     }
+    
+    private abstract class Invoker {
+        final DitaWriter writer;
+        final Method method;
+        final String attrName;
+        public Invoker(final String m, final String attrName, final Class<?>... args) throws Exception {
+            writer = new DitaWriter();
+            writer.setExtName(".dita");
+            final OutputUtils outputUtils = new OutputUtils();
+            outputUtils.setInputMapPathName(new File(srcDir, "main.ditamap").getAbsolutePath());
+            writer.setOutputUtils(outputUtils);        
+            method = DitaWriter.class.getDeclaredMethod(m, args);
+            method.setAccessible(true);
+            this.attrName = attrName;
+        }
+        public abstract String invoke(final String value) throws Exception;
+    }
+    
 }
