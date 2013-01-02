dita-ot/dita-ot

commit 2b5d4dc67538d4042f8b2872c8ca1f9fa2deb944

Move new flag support from plugin into base preprocess #1313

src/main/xsl/dita2html-base_template.xsl
@@ -42,6 +42,9 @@
 <!-- abbreviated-form domain -->
 <xsl:import href="xslhtml/abbrev-d.xsl"/>
 
+<!-- Integrate support for flagging with dita-ot pseudo-domain -->
+<xsl:include href="xslhtml/htmlflag.xsl"/>
+
 <dita:extension id="dita.xsl.xhtml" behavior="org.dita.dost.platform.ImportXSLAction" xmlns:dita="http://dita-ot.sourceforge.net"/>
 
 <!-- the dita to xhtml converter for element reference documents - not used now -->

src/main/plugins/net.sourceforge.dita-ot.flagging/antwrapper.xml
@@ -1,1 +1,0 @@
-<dummy><import file="build_flag.xml"/></dummy>

src/main/plugins/net.sourceforge.dita-ot.flagging/bats.xml
@@ -1,14 +1,0 @@
-﻿<?xml version="1.0" encoding="UTF-8"?><topic xmlns:ditaarch="http://dita.oasis-open.org/architecture/2005/" id="bats" xml:lang="en-us" ditaarch:DITAArchVersion="1.2" domains="(topic ui-d) (topic hi-d) (topic pr-d) (topic sw-d)                          (topic ut-d) (topic indexing-d) (topic ibmut-d)                          (topic delay-d)                          a(props activity)                          a(props architecture)                          a(props eclipsecategory)                          a(props operatingsystem)                          a(props plug-in)                          a(props windowingsystem)" class="- topic/topic "><title class="- topic/title ">Bats</title><shortdesc class="- topic/shortdesc ">Learn about your new bat.</shortdesc><prolog class="- topic/prolog "><metadata class="- topic/metadata "><keywords class="- topic/keywords "><keyword class="- topic/keyword ">ACME Bat Company</keyword></keywords></metadata><metadata otherprops="gen-kc" class="- topic/metadata "><othermeta class="- topic/othermeta " name="mainactivity" content=""/><othermeta class="- topic/othermeta " name="subactivity" content=""/><othermeta class="- topic/othermeta " name="otherprodlocation" content=""/><othermeta class="- topic/othermeta " name="prodid" content=""/><othermeta class="- topic/othermeta " name="prodversion" content=""/><othermeta class="- topic/othermeta " name="proddeployunit" content="any"/><othermeta class="- topic/othermeta " name="prodname" content=""/><othermeta class="- topic/othermeta " name="baseprodid" content=""/><othermeta class="- topic/othermeta " name="baseprodversion" content=""/><othermeta class="- topic/othermeta " name="baseprodname" content=""/><othermeta class="- topic/othermeta " name="otherprodid" content=""/><othermeta class="- topic/othermeta " name="otherprodversion" content=""/><othermeta class="- topic/othermeta " name="otherprodname" content=""/><othermeta class="- topic/othermeta " name="prodosfamily" content="any"/><othermeta class="- topic/othermeta " name="prodosname" content="any"/><othermeta class="- topic/othermeta " name="prodosedition" content="any"/><othermeta class="- topic/othermeta " name="prodosversion" content="any"/><othermeta class="- topic/othermeta " name="prodhardware" content="any"/><othermeta class="- topic/othermeta " name="softwareleveltype" content="release"/><othermeta class="- topic/othermeta " name="fixpackagetype" content=""/><othermeta class="- topic/othermeta " name="topic_status" content="complete"/></metadata></prolog><body class="- topic/body "><ul audience="someAud" platform="aix win" class="- topic/ul "><li rev="r1" class="- topic/li "><xref href="#bats/s" class="- topic/xref " type="syntaxdiagram">Figure 0</xref></li><li activity="deploy" rev="rev2" class="- topic/li "><xref href="bathistory.dita#bathistory/s" class="- topic/xref " type="syntaxdiagram">Figure 0</xref></li></ul><p id="wonder" class="- topic/p ">Bats are one of nature's wonders. A flying mammal that
-uses sound waves to locate flying insects in the dark. <xref keyref="peery" class="- topic/xref " scope="peer" href="peerpeer.dita#peery">blahhh</xref></p><pre id="s" class="- topic/pre " outputclass="syntaxdiagram"><?accSyntaxImage ./c.gif?><?accSyntaxHref bats_syn1.html?><?accSyntaxSkip Skip visual syntax diagram?><?accSyntaxRead Read syntax diagram?>
-This is a titled syntax
-
->>-+-a-+------------------------------------------------------->&lt;
-   +-b-+   
-   '-c-'   
-
-</pre><fig id="figbat" class="- topic/fig "><title class="- topic/title ">Your cute, furry bat </title><image href="batpic.gif" longdescref="batpicdesc.dita" placement="break" class="- topic/image "><alt class="- topic/alt ">Picture of my cute bat being held</alt></image></fig><p id="thanks" class="- topic/p ">Thank-you for your purchase of the ACME Home Bat kit.
-We hope that it gives you and your family lots of wholesome fun.</p><note type="attention" class="- topic/note ">Ensure that you handle your bat while wearing
-welding gloves. You do not want to get bitten.</note><p class="- topic/p ">Use <tm tmclass="IBM" tmtype="tm" trademark="Anyplace" class="- topic/tm ">Anyplace</tm> to quickly
-determine your bat's location; this will help you care for it: <xref href="batcaring.dita" keyref="atopic" scope="peer" class="- topic/xref "/>. The <tm tmclass="IBM" tmtype="tm" trademark="HyperSwap" class="- topic/tm ">HyperSwap</tm> will help your bat
-return home if it ever gets loose.</p></body><related-links class="- topic/related-links "><linkpool class="- topic/linkpool "><linkpool class="- topic/linkpool "><link class="- topic/link " format="dita" type="task" href="batcaring.dita" role="child"><linktext class="- topic/linktext ">Caring for your fruit bat</linktext><desc class="- topic/desc ">Learn how to care for your new pet bat.</desc></link><link class="- topic/link " format="dita" type="task" href="batcleaning.dita" role="child"><linktext class="- topic/linktext ">Cleaning your fruit bat</linktext><desc class="- topic/desc ">Ensure your bat stays clean and healthy.</desc></link><link class="- topic/link " format="dita" type="task" href="batfeeding.dita" role="child"><linktext class="- topic/linktext ">Feeding your bat</linktext><desc class="- topic/desc ">This topic describes how you can feed your new bat!</desc></link><link class="- topic/link " format="dita" type="concept" href="batsonar.dita" role="child"><linktext class="- topic/linktext ">Bat sonar</linktext><desc class="- topic/desc ">Learn how bats find their way in the dark!</desc></link><link class="- topic/link " format="dita" type="reference" href="batguano.dita" role="child"><linktext class="- topic/linktext ">Bat guano</linktext><desc class="- topic/desc ">This describes the uses and chemical nature of bat droppings.</desc></link><link class="- topic/link " format="dita" type="reference" href="bathistory.dita" role="child"><linktext class="- topic/linktext ">Bat history</linktext><desc class="- topic/desc ">Learn about the valuable role that bats have played in
-history</desc></link><link class="- topic/link " format="dita" type="topic" href="batreuse.dita" role="child"><linktext class="- topic/linktext ">Bat recycling</linktext><desc class="- topic/desc ">Learn about reusing parts of your bat files.</desc></link></linkpool></linkpool></related-links></topic>

src/main/plugins/net.sourceforge.dita-ot.flagging/build_flag.xml
@@ -1,84 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!-- This file is part of the DITA Open Toolkit project hosted on 
-     Sourceforge.net. See the accompanying license.txt file for 
-     applicable licenses.-->
-<!-- (c) Copyright IBM Corp. 2012 All Rights Reserved. -->
-<project name="flag">
-	
-
-    <target name="preprocess_flagging" description="Add flagging information to topics" if="dita.input.valfile">
-      <makeurl file="${dita.input.valfile}" property="dita.input.filterfile.url" validate="no"></makeurl>
-      <xslt taskname="preprocess_flag" 
-          basedir="${dita.temp.dir}" 
-          destdir="${dita.temp.dir}" 
-          includesfile="${dita.temp.dir}/${fullditatopicfile}" 
-          classpathref="dost.class.path" 
-          reloadstylesheet="${dita.preprocess.reloadstylesheet}" 
-          style="${dita.plugin.net.sourceforge.dita-ot.flagging.dir}/xsl/flag.xsl"
-          filenameparameter="FILENAME" 
-          filedirparameter="FILEDIR">
-        <excludesfile name="${dita.temp.dir}${file.separator}${resourceonlyfile}" if="resourceonlyfile"></excludesfile>
-        <includesfile name="${dita.temp.dir}/${chunkedtopicfile}" if="chunkedtopicfile"></includesfile>
-
-        <param name="TRANSTYPE" expression="${transtype}"></param>
-        <param name="DITAEXT" expression="${dita.ext}" if="dita.ext"></param>
-        <param name="FILTERFILE" expression="${dita.input.filterfile.url}"></param>
-        <param name="DRAFT" expression="${args.draft}" if="args.draft"></param>
-        <param name="BASEDIR" expression="${basedir}"></param>
-        <param name="OUTPUTDIR" expression="${output.dir}"></param>
-        <param name="DBG" expression="${args.debug}" if="args.debug"></param>
-
-        <mapper type="glob" from="*" to="*.flag"></mapper>
-        <xmlcatalog refid="dita.catalog"></xmlcatalog>
-      </xslt>
-      <move todir="${dita.temp.dir}">
-        <fileset dir="${dita.temp.dir}" includes="**/*.flag"></fileset>
-        <mapper type="glob" from="*.flag" to="*"></mapper>
-      </move>
-    </target>
-    
-
-  	
-    <target name="dita.out.map.xhtml.tocBLAH" unless="noMap" if="inner.transform" description="Build HTML TOC file,which will adjust the directory">
-        <xslt basedir="${dita.temp.dir}" destdir="${output.dir}" includesfile="${dita.temp.dir}${file.separator}${user.input.file.listfile}" classpathref="dost.class.path" style="${args.xhtml.toc.xsl}">
-        	<excludesfile name="${dita.temp.dir}${file.separator}${resourceonlyfile}" if="resourceonlyfile"></excludesfile>
-            <param name="DITAEXT" expression="${dita.ext}" if="dita.ext"></param>
-            <param name="OUTEXT" expression="${out.ext}" if="out.ext"></param>
-            <param name="contenttarget" expression="${args.xhtml.contenttarget}" if="args.xhtml.contenttarget"></param>
-            <param name="CSS" expression="${args.css.file}" if="args.css.file"></param>
-            <param name="CSSPATH" expression="${user.csspath}" if="user.csspath"></param>
-            <param name="OUTPUTCLASS" expression="${args.xhtml.toc.class}" if="args.xhtml.toc.class"></param>
-            
-            <mapper type="glob" from="${user.input.file}" to="${args.xhtml.toc}${out.ext}"></mapper>
-          <xmlcatalog refid="dita.catalog"></xmlcatalog>
-        </xslt>
-    </target>
-    
-  <target name="copy-revflag" if="dita.input.valfile">
-    <dita-ot-echo id="DOTA069W">
-      <param name="1" value="copy-revflag"></param>
-    </dita-ot-echo>
-  </target>
-    
-  <target name="copy-css" unless="user.csspath.url" description="Copy CSS files">
-    <condition property="user.copycss.yes">
-      <and>
-        <equals arg1="${args.copycss}" arg2="yes"></equals>
-        <isset property="args.css.present"></isset>
-      </and>
-    </condition>
-    <property name="user.csspath.real" location="${output.dir}/${user.csspath}"></property>
-    <mkdir dir="${user.csspath.real}"></mkdir>
-    <!-- Always copy system default css files -->
-    <copy todir="${user.csspath.real}">
-      <fileset dir="${dita.resource.dir}" includes="*.css"></fileset>
-    </copy>
-    <!-- Copy user specify css file when required -->
-    <antcall target="copy-css-user"></antcall>
-  </target>
-
-  <target name="copy-css-user" if="user.copycss.yes">
-    <copy file="${args.css.real}" todir="${user.csspath.real}"></copy>
-  </target>
-  
-</project>

src/main/plugins/net.sourceforge.dita-ot.flagging/del.xml
@@ -1,116 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<topic xmlns:ditaarch="http://dita.oasis-open.org/architecture/2005/" id="bats"
-       xml:lang="en-us"
-       ditaarch:DITAArchVersion="1.2"
-       domains="(topic ui-d) (topic hi-d) (topic pr-d) (topic sw-d)                          (topic ut-d) (topic indexing-d) (topic ibmut-d)                          (topic delay-d)                          a(props activity)                          a(props architecture)                          a(props eclipsecategory)                          a(props operatingsystem)                          a(props plug-in)                          a(props windowingsystem)"
-       class="- topic/topic ">
-   <title class="- topic/title ">Bats</title>
-   <shortdesc class="- topic/shortdesc ">Learn about your new bat.</shortdesc>
-   <prolog class="- topic/prolog ">
-      <metadata class="- topic/metadata ">
-         <keywords class="- topic/keywords ">
-            <keyword class="- topic/keyword ">ACME Bat Company</keyword>
-         </keywords>
-      </metadata>
-      <metadata otherprops="gen-kc" class="- topic/metadata ">
-         <othermeta class="- topic/othermeta " name="mainactivity" content=""/>
-         <othermeta class="- topic/othermeta " name="subactivity" content=""/>
-         <othermeta class="- topic/othermeta " name="otherprodlocation" content=""/>
-         <othermeta class="- topic/othermeta " name="prodid" content=""/>
-         <othermeta class="- topic/othermeta " name="prodversion" content=""/>
-         <othermeta class="- topic/othermeta " name="proddeployunit" content="any"/>
-         <othermeta class="- topic/othermeta " name="prodname" content=""/>
-         <othermeta class="- topic/othermeta " name="baseprodid" content=""/>
-         <othermeta class="- topic/othermeta " name="baseprodversion" content=""/>
-         <othermeta class="- topic/othermeta " name="baseprodname" content=""/>
-         <othermeta class="- topic/othermeta " name="otherprodid" content=""/>
-         <othermeta class="- topic/othermeta " name="otherprodversion" content=""/>
-         <othermeta class="- topic/othermeta " name="otherprodname" content=""/>
-         <othermeta class="- topic/othermeta " name="prodosfamily" content="any"/>
-         <othermeta class="- topic/othermeta " name="prodosname" content="any"/>
-         <othermeta class="- topic/othermeta " name="prodosedition" content="any"/>
-         <othermeta class="- topic/othermeta " name="prodosversion" content="any"/>
-         <othermeta class="- topic/othermeta " name="prodhardware" content="any"/>
-         <othermeta class="- topic/othermeta " name="softwareleveltype" content="release"/>
-         <othermeta class="- topic/othermeta " name="fixpackagetype" content=""/>
-         <othermeta class="- topic/othermeta " name="topic_status" content="complete"/>
-      </metadata>
-   </prolog>
-   <body class="- topic/body ">
-      <ul audience="someAud" platform="aix win" class="- topic/ul ">
-         <li rev="r1" class="- topic/li ">
-            <xref href="#bats/s" class="- topic/xref " type="syntaxdiagram">Figure 0</xref>
-         </li>
-         <li activity="deploy" rev="rev2" class="- topic/li ">
-            <xref href="bathistory.dita#bathistory/s" class="- topic/xref " type="syntaxdiagram">Figure 0</xref>
-         </li>
-      </ul>
-      <p id="wonder" class="- topic/p ">Bats are one of nature's wonders. A flying mammal that
-uses sound waves to locate flying insects in the dark. <xref keyref="peery" class="- topic/xref " scope="peer" href="peerpeer.dita#peery">blahhh</xref>
-      </p>
-      <pre id="s" class="- topic/pre " outputclass="syntaxdiagram"><?accSyntaxImage ./c.gif?><?accSyntaxHref bats_syn1.html?><?accSyntaxSkip Skip visual syntax diagram?><?accSyntaxRead Read syntax diagram?>
-This is a titled syntax
-
-&gt;&gt;-+-a-+-------------------------------------------------------&gt;&lt;
-?ÿ?ÿ?ÿ+-b-+?ÿ?ÿ?ÿ
-?ÿ?ÿ?ÿ'-c-'?ÿ?ÿ?ÿ
-
-</pre>
-      <fig id="figbat" class="- topic/fig ">
-         <title class="- topic/title ">Your cute, furry bat </title>
-         <image href="batpic.gif" longdescref="batpicdesc.dita" placement="break"
-                class="- topic/image ">
-            <alt class="- topic/alt ">Picture of my cute bat being held</alt>
-         </image>
-      </fig>
-      <p id="thanks" class="- topic/p ">Thank-you for your purchase of the ACME Home Bat kit.
-We hope that it gives you and your family lots of wholesome fun.</p>
-      <note type="attention" class="- topic/note ">Ensure that you handle your bat while wearing
-welding gloves. You do not want to get bitten.</note>
-      <p class="- topic/p ">Use <tm tmclass="IBM" tmtype="tm" trademark="Anyplace" class="- topic/tm ">Anyplace</tm> to quickly
-determine your bat's location; this will help you care for it: <xref href="batcaring.dita" keyref="atopic" scope="peer" class="- topic/xref "/>. The <tm tmclass="IBM" tmtype="tm" trademark="HyperSwap" class="- topic/tm ">HyperSwap</tm> will help your bat
-return home if it ever gets loose.</p>
-   </body>
-   <related-links class="- topic/related-links ">
-      <linkpool class="- topic/linkpool ">
-         <linkpool class="- topic/linkpool ">
-            <link class="- topic/link " format="dita" type="task" href="batcaring.dita"
-                  role="child">
-               <linktext class="- topic/linktext ">Caring for your fruit bat</linktext>
-               <desc class="- topic/desc ">Learn how to care for your new pet bat.</desc>
-            </link>
-            <link class="- topic/link " format="dita" type="task" href="batcleaning.dita"
-                  role="child">
-               <linktext class="- topic/linktext ">Cleaning your fruit bat</linktext>
-               <desc class="- topic/desc ">Ensure your bat stays clean and healthy.</desc>
-            </link>
-            <link class="- topic/link " format="dita" type="task" href="batfeeding.dita"
-                  role="child">
-               <linktext class="- topic/linktext ">Feeding your bat</linktext>
-               <desc class="- topic/desc ">This topic describes how you can feed your new bat!</desc>
-            </link>
-            <link class="- topic/link " format="dita" type="concept" href="batsonar.dita"
-                  role="child">
-               <linktext class="- topic/linktext ">Bat sonar</linktext>
-               <desc class="- topic/desc ">Learn how bats find their way in the dark!</desc>
-            </link>
-            <link class="- topic/link " format="dita" type="reference" href="batguano.dita"
-                  role="child">
-               <linktext class="- topic/linktext ">Bat guano</linktext>
-               <desc class="- topic/desc ">This describes the uses and chemical nature of bat droppings.</desc>
-            </link>
-            <link class="- topic/link " format="dita" type="reference" href="bathistory.dita"
-                  role="child">
-               <linktext class="- topic/linktext ">Bat history</linktext>
-               <desc class="- topic/desc ">Learn about the valuable role that bats have played in
-history</desc>
-            </link>
-            <link class="- topic/link " format="dita" type="topic" href="batreuse.dita"
-                  role="child">
-               <linktext class="- topic/linktext ">Bat recycling</linktext>
-               <desc class="- topic/desc ">Learn about reusing parts of your bat files.</desc>
-            </link>
-         </linkpool>
-      </linkpool>
-   </related-links>
-</topic>

src/main/plugins/net.sourceforge.dita-ot.flagging/plugin.xml
@@ -1,12 +1,0 @@
-<!-- 
-  This file is part of the DITA Open Toolkit project hosted on
-  Sourceforge.net. See the accompanying license.txt file for
-  applicable licenses.
-  
-  (C) Copyright Shawn McKenzie, 2007. All Rights Reserved.
-  *-->
-<plugin id="net.sourceforge.dita-ot.flagging">
-  <feature extension="dita.conductor.target.relative" file="antwrapper.xml"/>
-  <feature extension="depend.preprocess.post" value="preprocess_flagging"/>
-  <feature extension="dita.xsl.xhtml" file="xsl/htmlflag.xsl"/>
-</plugin>

src/main/plugins/net.sourceforge.dita-ot.flagging/some.ditaval
@@ -1,12 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?><val>
-<style-conflict background-conflict-color="red"/>
-   <prop action="flag" att="audience" style="underline" val="someAud" backcolor="green"/>
-   <prop action="flag" att="platform" val="aix" style="overline" backcolor="blue">
-      <startflag imageref="aix.gif"><alt-text>startaix</alt-text></startflag>
-      
-      <endflag imageref="endaix.gif"><alt-text>endaix</alt-text></endflag></prop>
-   <revprop action="flag" color="aqua" val="r1"/>
-   <prop action="flag" att="activity" color="blue" val="deploy"/>
-   <revprop action="flag" style="double-underline" val="rev2">
-      <startflag imageref="startmeflag.gif"><alt-text>ssssssssssstart</alt-text></startflag>
-      <endflag imageref="endmeflag.gif"><alt-text>eeeeeeeeeeeeeend</alt-text></endflag></revprop></val>

src/main/plugins/net.sourceforge.dita-ot.flagging/xsl/flag.xsl
@@ -1,21 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!-- This file is part of the DITA Open Toolkit project hosted on 
- Sourceforge.net. See the accompanying license.txt file for 
- applicable licenses.-->
-<!-- (c) Copyright IBM Corp. 2012 -->
-<!-- PURPOSE: Replace the XHTML based flagging routines with a common routine.
-     Logic for determining what to flag is the same.
-     When flags are active:
-     * For images: can have multiple per element, and the ditaval information is well structured.
-                   Copy in active ditaval prop and revprop information as sub-elements,
-                   nested within a pseudo-foreign specialization.
-     * For styles: only one style can be set per element. Because we already have code to calculate
-                   CSS style here, and many output formats can potentially use that, add it
-                   as @outputclass on the startprop <foreign> specialization
-              -->
-<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" 
-  version="1.0">
-
-  <xsl:import href="flagImpl.xsl"/>
-
-</xsl:stylesheet>

commit e37ad9bc3c0e209ea69c03c62431876a4ca64712

Fix XHTML base stylesheet include order #1425
The bug was introduced in commit 2b5d4dc6.

src/main/xsl/dita2html-base_template.xsl
@@ -42,13 +42,13 @@
 <!-- abbreviated-form domain -->
 <xsl:import href="xslhtml/abbrev-d.xsl"/>
 
-<!-- Integrate support for flagging with dita-ot pseudo-domain -->
-<xsl:include href="xslhtml/htmlflag.xsl"/>
-
 <dita:extension id="dita.xsl.xhtml" behavior="org.dita.dost.platform.ImportXSLAction" xmlns:dita="http://dita-ot.sourceforge.net"/>
 
 <!-- the dita to xhtml converter for element reference documents - not used now -->
 <!--<xsl:import href="elementrefdisp.xsl"/>-->
+
+<!-- Integrate support for flagging with dita-ot pseudo-domain -->
+<xsl:include href="xslhtml/htmlflag.xsl"/>  
 
 <!-- DITAEXT file extension name of dita topic file -->
 <xsl:param name="DITAEXT" select="'.xml'"/>    