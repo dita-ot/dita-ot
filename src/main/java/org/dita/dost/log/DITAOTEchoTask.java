dita-ot/dita-ot

commit 040aa1804395c91fb6b016ca1f8873355f2bfbea

Clean logging #1471
To make transition to other logging frameworks easier, reactor logging:

* Remove support for Properties as message arguments
* Replace logException with logError
* Remove logFatal and logException

src/main/java/org/dita/dost/log/DITAOTEchoTask.java
@@ -26,7 +26,6 @@ import org.dita.dost.log.DITAOTAntLogger;
 public final class DITAOTEchoTask extends Echo {
     private String id = null;
 
-    private final Properties prop = new Properties();
     /** Nested params. */
     private final ArrayList<Param> params = new ArrayList<Param>();
     private DITAOTLogger logger;
@@ -63,6 +62,7 @@ public final class DITAOTEchoTask extends Echo {
      */
     @Override
     public void execute() throws BuildException {
+        final ArrayList<String> prop = new ArrayList<String>();
         for (final Param p : params) {
             if (!p.isValid()) {
                 throw new BuildException("Incomplete parameter");
@@ -71,17 +71,16 @@ public final class DITAOTEchoTask extends Echo {
             final String unlessProperty = p.getUnless();
             if ((ifProperty == null || getProject().getProperties().containsKey(ifProperty))
                     && (unlessProperty == null || !getProject().getProperties().containsKey(unlessProperty))) {
-                prop.put("%" + p.getName(), p.getValue());
+                final int idx = Integer.parseInt(p.getName()) - 1;
+                prop.set(idx, p.getValue());
             }
         }
         
         logger = new DITAOTAntLogger(getProject());
-        final MessageBean msgBean = MessageUtils.getInstance().getMessage(id, prop);
+        final MessageBean msgBean = MessageUtils.getInstance().getMessage(id, prop.toArray(new String[prop.size()]));
         if (msgBean != null) {
             final String type = msgBean.getType();
-            if(FATAL.equals(type)){
-                logger.logFatal(msgBean.toString());
-            } else if(ERROR.equals(type)){
+            if(ERROR.equals(type)){
                 logger.logError(msgBean.toString());
             } else if(WARN.equals(type)){
                 logger.logWarn(msgBean.toString());

src/main/java/org/dita/dost/log/DITAOTFailTask.java
@@ -26,7 +26,6 @@ import org.dita.dost.invoker.ExtensibleAntInvoker.Param;
 public final class DITAOTFailTask extends Exit {
     private String id = null;
 
-    private final Properties prop = new Properties();
     /** Nested params. */
     private final ArrayList<Param> params = new ArrayList<Param>();
 
@@ -74,6 +73,7 @@ public final class DITAOTFailTask extends Exit {
         if (id == null) {
             throw new BuildException("id attribute must be specified");
         }
+        final ArrayList<String> prop = new ArrayList<String>();
         for (final Param p : params) {
             if (!p.isValid()) {
                 throw new BuildException("Incomplete parameter");
@@ -82,11 +82,12 @@ public final class DITAOTFailTask extends Exit {
             final String unlessProperty = p.getUnless();
             if ((ifProperty == null || getProject().getProperties().containsKey(ifProperty))
                     && (unlessProperty == null || !getProject().getProperties().containsKey(unlessProperty))) {
-                prop.put("%" + p.getName(), p.getValue());
+                final int idx = Integer.parseInt(p.getName()) - 1;
+                prop.set(idx, p.getValue());
             }
         }
         
-        final MessageBean msgBean = MessageUtils.getInstance().getMessage(id, prop);
+        final MessageBean msgBean = MessageUtils.getInstance().getMessage(id, prop.toArray(new String[prop.size()]));
         final DITAOTLogger logger = new DITAOTAntLogger(getProject());
         if (msgBean != null) {
             final String type = msgBean.getType();

commit 4b30fa24e4d3d73d888e51e0534e991f1236da32

Fix logging Ant task bugs
The bug was introduced in 040aa1804395c91fb6b016ca1f8873355f2bfbea.

src/main/java/org/dita/dost/log/DITAOTEchoTask.java
@@ -62,22 +62,8 @@ public final class DITAOTEchoTask extends Echo {
      */
     @Override
     public void execute() throws BuildException {
-        final ArrayList<String> prop = new ArrayList<String>();
-        for (final Param p : params) {
-            if (!p.isValid()) {
-                throw new BuildException("Incomplete parameter");
-            }
-            final String ifProperty = p.getIf();
-            final String unlessProperty = p.getUnless();
-            if ((ifProperty == null || getProject().getProperties().containsKey(ifProperty))
-                    && (unlessProperty == null || !getProject().getProperties().containsKey(unlessProperty))) {
-                final int idx = Integer.parseInt(p.getName()) - 1;
-                prop.set(idx, p.getValue());
-            }
-        }
-        
         logger = new DITAOTAntLogger(getProject());
-        final MessageBean msgBean = MessageUtils.getInstance().getMessage(id, prop.toArray(new String[prop.size()]));
+        final MessageBean msgBean = MessageUtils.getInstance().getMessage(id, readParamValues());
         if (msgBean != null) {
             final String type = msgBean.getType();
             if(ERROR.equals(type)){
@@ -92,4 +78,32 @@ public final class DITAOTEchoTask extends Echo {
         }
     }
 
+    /**
+     * Read parameter values to an array.
+     * 
+     * @return parameter values where array index corresponds to parameter name
+     */
+    private String[] readParamValues() throws BuildException {
+        final ArrayList<String> prop = new ArrayList<String>();
+        for (final Param p : params) {
+            if (!p.isValid()) {
+                throw new BuildException("Incomplete parameter");
+            }
+            final String ifProperty = p.getIf();
+            final String unlessProperty = p.getUnless();
+            if ((ifProperty == null || getProject().getProperties().containsKey(ifProperty))
+                    && (unlessProperty == null || !getProject().getProperties().containsKey(unlessProperty))) {
+                final int idx = Integer.parseInt(p.getName()) - 1;
+                if (idx >= prop.size()) {
+                    prop.ensureCapacity(idx + 1);
+                    while (prop.size() < idx + 1) {
+                        prop.add(null);
+                    }
+                }
+                prop.set(idx, p.getValue());
+            }
+        }
+        return prop.toArray(new String[prop.size()]);
+    }
+    
 }

src/main/java/org/dita/dost/log/DITAOTFailTask.java
@@ -73,21 +73,8 @@ public final class DITAOTFailTask extends Exit {
         if (id == null) {
             throw new BuildException("id attribute must be specified");
         }
-        final ArrayList<String> prop = new ArrayList<String>();
-        for (final Param p : params) {
-            if (!p.isValid()) {
-                throw new BuildException("Incomplete parameter");
-            }
-            final String ifProperty = p.getIf();
-            final String unlessProperty = p.getUnless();
-            if ((ifProperty == null || getProject().getProperties().containsKey(ifProperty))
-                    && (unlessProperty == null || !getProject().getProperties().containsKey(unlessProperty))) {
-                final int idx = Integer.parseInt(p.getName()) - 1;
-                prop.set(idx, p.getValue());
-            }
-        }
         
-        final MessageBean msgBean = MessageUtils.getInstance().getMessage(id, prop.toArray(new String[prop.size()]));
+        final MessageBean msgBean = MessageUtils.getInstance().getMessage(id, readParamValues());
         final DITAOTLogger logger = new DITAOTAntLogger(getProject());
         if (msgBean != null) {
             final String type = msgBean.getType();
@@ -110,6 +97,34 @@ public final class DITAOTFailTask extends Exit {
         }
         
         
+    }
+
+    /**
+     * Read parameter values to an array.
+     * 
+     * @return parameter values where array index corresponds to parameter name
+     */
+    private String[] readParamValues() throws BuildException {
+        final ArrayList<String> prop = new ArrayList<String>();
+        for (final Param p : params) {
+            if (!p.isValid()) {
+                throw new BuildException("Incomplete parameter");
+            }
+            final String ifProperty = p.getIf();
+            final String unlessProperty = p.getUnless();
+            if ((ifProperty == null || getProject().getProperties().containsKey(ifProperty))
+                    && (unlessProperty == null || !getProject().getProperties().containsKey(unlessProperty))) {
+                final int idx = Integer.parseInt(p.getName()) - 1;
+                if (idx >= prop.size()) {
+                    prop.ensureCapacity(idx + 1);
+                    while (prop.size() < idx + 1) {
+                        prop.add(null);
+                    }
+                }
+                prop.set(idx, p.getValue());
+            }
+        }
+        return prop.toArray(new String[prop.size()]);
     }
     
     // Ant Exit class methods --------------------------------------------------