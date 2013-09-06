dita-ot/dita-ot

commit 071a925c3677998cca992d74da8262eb41f8348c

Set file info properties directly instead of with file list #1544

src/main/java/org/dita/dost/module/ChunkModule.java
@@ -36,6 +36,7 @@ import org.dita.dost.util.Configuration;
 import org.dita.dost.util.FileUtils;
 import org.dita.dost.util.Job;
 import org.dita.dost.util.StringUtils;
+import org.dita.dost.util.Job.FileInfo;
 import org.dita.dost.writer.TopicRefWriter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -281,24 +282,38 @@ final public class ChunkModule implements AbstractPipelineModule {
             }
         }
 
-        //TODO Remove newly generated files from resource-only list, these new files should not
-        //     excluded from the final outputs.
-        final Set<String> resourceOnlySet = job.getSet(RESOURCE_ONLY_LIST);
-        resourceOnlySet.removeAll(chunkedTopicSet);
-        resourceOnlySet.removeAll(chunkedDitamapSet);
-
-        job.setSet(RESOURCE_ONLY_LIST, resourceOnlySet);
-        job.setSet(FULL_DITA_TOPIC_LIST, topicList);
-        job.setSet(FULL_DITAMAP_LIST, ditamapList);
-        topicList.addAll(ditamapList);
-        job.setSet(FULL_DITAMAP_TOPIC_LIST, topicList);
-
+        for (final String file: chunkedTopicSet) {
+            job.getOrCreateFileInfo(file).isResourceOnly = false;
+        }
+        for (final String file: chunkedDitamapSet) {
+            job.getOrCreateFileInfo(file).isResourceOnly = false;
+        }
+        for (FileInfo f: job.getFileInfo().values()) {
+            if ("dita".equals(f.format) || "ditamap".equals(f.format)) {
+                f.isActive = false;
+            }
+        }
+        for (final String file: topicList) {
+            final FileInfo ff = job.getOrCreateFileInfo(file);
+            ff.format = "dita";
+            ff.isActive = true;
+        }
+        for (final String file: ditamapList) {
+            final FileInfo ff = job.getOrCreateFileInfo(file);
+            ff.format = "ditamap";
+            ff.isActive = true;
+        }
+        
         job.setProperty("chunkedditamapfile", CHUNKED_DITAMAP_LIST_FILE);
         job.setProperty("chunkedtopicfile", CHUNKED_TOPIC_LIST_FILE);
         job.setProperty("resourceonlyfile", RESOURCE_ONLY_LIST_FILE);
 
-        job.setSet(CHUNKED_DITAMAP_LIST, chunkedDitamapSet);
-        job.setSet(CHUNKED_TOPIC_LIST, chunkedTopicSet);
+        for (final String file: chunkedDitamapSet) {
+            job.getOrCreateFileInfo(file).isChunkedDitaMap = true;
+        }
+        for (final String file: chunkedDitamapSet) {
+            job.getOrCreateFileInfo(file).isChunked = true;
+        }
 
         try{
             job.write();

src/main/java/org/dita/dost/writer/ChunkTopicParser.java
@@ -579,7 +579,9 @@ public final class ChunkTopicParser extends AbstractXMLWriter {
             //the @href value are added in fullditatopic and fullditamapandtopic,
             //while they are not supposed to be contained, so should be be removed
 
-            job.setSet(COPYTO_SOURCE_LIST, copytoSource);
+            for (final String file: copytoSource) {
+                job.getOrCreateFileInfo(file).isCopyToSource = true;
+            }
 
             job.setMap(COPYTO_TARGET_TO_SOURCE_MAP_LIST, copytotarget2source);
 

src/main/java/org/dita/dost/writer/ConrefPushParser.java
@@ -237,9 +237,7 @@ public final class ConrefPushParser extends AbstractXMLWriter {
                     return;
                 }
             }
-            final Set<String> stringBuffer = new HashSet<String>(job.getSet(CONREF_LIST));
-            stringBuffer.add(reletivePath);
-            job.setSet(CONREF_LIST, stringBuffer);
+            job.getOrCreateFileInfo(reletivePath).hasConref = true;
             
             job.write();
 

commit 24977a72765ccbf7dd483eb52987f28cefcb68be

Fix list generation bug in chunking
Bug was introduced in commit 071a925.

src/main/java/org/dita/dost/module/ChunkModule.java
@@ -313,7 +313,7 @@ final public class ChunkModule implements AbstractPipelineModule {
         for (final String file: chunkedDitamapSet) {
             job.getOrCreateFileInfo(file).isChunkedDitaMap = true;
         }
-        for (final String file: chunkedDitamapSet) {
+        for (final String file: chunkedTopicSet) {
             job.getOrCreateFileInfo(file).isChunked = true;
         }
 