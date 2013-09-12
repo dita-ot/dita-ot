dita-ot/dita-ot

commit 9f546398c04fbd4a9abddb0d80377282fe4d8d6c

Add getter for file info collection

src/main/java/org/dita/dost/util/Job.java
@@ -485,12 +485,21 @@ public final class Job {
     }
 
     /**
+     * Get all file info objects as a map
+     * 
+     * @return map of file info objects, where the key is the {@link FileInfo#file} value. May be empty
+     */
+    public Map<String, FileInfo> getFileInfoMap() {
+        return Collections.unmodifiableMap(new HashMap<String, FileInfo>(files));
+    }
+    
+    /**
      * Get all file info objects
      * 
-     * @return map of file info objects, where the key is the {@link FileInfo#file} value
+     * @return collection of file info objects, may be empty
      */
-    public Map<String, FileInfo> getFileInfo() {
-        return Collections.unmodifiableMap(new HashMap<String, FileInfo>(files));
+    public Collection<FileInfo> getFileInfo() {
+        return Collections.unmodifiableCollection(files.values());
     }
     
     /**

src/main/java/org/dita/dost/module/ChunkModule.java
@@ -105,7 +105,7 @@ final public class ChunkModule implements AbstractPipelineModule {
             final Document doc = builder.parse(new File(mapFile));
             final Element root = doc.getDocumentElement();
             if(root.getAttribute(ATTRIBUTE_NAME_CLASS).contains(" eclipsemap/plugin ") && transtype.equals(INDEX_TYPE_ECLIPSEHELP)){
-                for (final FileInfo f: job.getFileInfo().values()) {
+                for (final FileInfo f: job.getFileInfo()) {
                     if (f.isActive && ATTR_FORMAT_VALUE_DITAMAP.equals(f.format)) {
                         mapFile = new File(tempDir, f.file).getAbsolutePath();
                         mapReader.read(mapFile);
@@ -149,7 +149,7 @@ final public class ChunkModule implements AbstractPipelineModule {
         topicRefWriter.setChangeTable(changeTable);
         topicRefWriter.setup(conflictTable);
         try{
-            for (final FileInfo f: job.getFileInfo().values()) {
+            for (final FileInfo f: job.getFileInfo()) {
                 if (f.isActive && (ATTR_FORMAT_VALUE_DITA.equals(f.format) || ATTR_FORMAT_VALUE_DITAMAP.equals(f.format))) {
                     topicRefWriter.write(tempDir.getAbsoluteFile(), new File(f.file), relativePath2fix);
                 }
@@ -175,12 +175,12 @@ final public class ChunkModule implements AbstractPipelineModule {
         }
 
         final Set<String> hrefTopics = new HashSet<String>();
-        for (final FileInfo f: job.getFileInfo().values()) {
+        for (final FileInfo f: job.getFileInfo()) {
             if (f.isNonConrefTarget) {
                 hrefTopics.add(f.file);
             }
         }
-        for (final FileInfo f: job.getFileInfo().values()) {
+        for (final FileInfo f: job.getFileInfo()) {
             if (f.isSkipChunk) {
                 final String s = f.file;
                 if (!StringUtils.isEmptyString(s) && getFragment(s) == null) {
@@ -204,7 +204,7 @@ final public class ChunkModule implements AbstractPipelineModule {
 
         final Set<String> topicList = new LinkedHashSet<String>(INT_128);
         final Set<String> oldTopicList = new HashSet<String>();
-        for (final FileInfo f: job.getFileInfo().values()) {
+        for (final FileInfo f: job.getFileInfo()) {
             if (f.isActive && ATTR_FORMAT_VALUE_DITA.equals(f.format)) {
                 oldTopicList.add(f.file);
             }
@@ -223,7 +223,7 @@ final public class ChunkModule implements AbstractPipelineModule {
         final Set<String> chunkedTopicSet=new LinkedHashSet<String>(INT_128);
         final Set<String> chunkedDitamapSet=new LinkedHashSet<String>(INT_128);
         final Set<String> ditamapList = new HashSet<String>();
-        for (final FileInfo f: job.getFileInfo().values()) {
+        for (final FileInfo f: job.getFileInfo()) {
             if (f.isActive && ATTR_FORMAT_VALUE_DITAMAP.equals(f.format)) {
                 ditamapList.add(f.file);
             }
@@ -303,7 +303,7 @@ final public class ChunkModule implements AbstractPipelineModule {
             }
         }
 
-        for (FileInfo f: job.getFileInfo().values()) {
+        for (FileInfo f: job.getFileInfo()) {
             if (ATTR_FORMAT_VALUE_DITA.equals(f.format) || ATTR_FORMAT_VALUE_DITAMAP.equals(f.format)) {
                 f.isActive = false;
             }

src/main/java/org/dita/dost/module/CoderefModule.java
@@ -67,7 +67,7 @@ final class CoderefModule implements AbstractPipelineModule {
 
         final CoderefResolver writer = new CoderefResolver();
         writer.setLogger(logger);
-        for (final FileInfo f: job.getFileInfo().values()) {
+        for (final FileInfo f: job.getFileInfo()) {
             if (f.hasCoderef) {
                 //FIXME:This writer deletes and renames files, have to
                 writer.write(new File(tempDir, f.file).getAbsolutePath());

src/main/java/org/dita/dost/module/ConrefPushModule.java
@@ -64,7 +64,7 @@ final class ConrefPushModule implements AbstractPipelineModule {
 
         final ConrefPushReader reader = new ConrefPushReader();
         reader.setLogger(logger);
-        for(final FileInfo f: job.getFileInfo().values()) {
+        for(final FileInfo f: job.getFileInfo()) {
             if (f.isConrefPush) {
                 final File file = new File(tempDir, f.file);
                 logger.logInfo("Reading  " + file.getAbsolutePath());

src/main/java/org/dita/dost/module/DebugAndFilterModule.java
@@ -180,7 +180,7 @@ final class DebugAndFilterModule implements AbstractPipelineModule {
 
             final Map<String, Set<String>> dic = readMapFromXML(FILE_NAME_SUBJECT_DICTIONARY);
 
-            for (final FileInfo f: job.getFileInfo().values()) {
+            for (final FileInfo f: job.getFileInfo()) {
                 if ((f.isActive && ("dita".equals(f.format) || "ditamap".equals(f.format)))
                         || f.isConrefTarget || f.isCopyToSource) {
                     final String filename = f.file;

src/main/java/org/dita/dost/module/FilterModule.java
@@ -51,7 +51,7 @@ final class FilterModule implements AbstractPipelineModule {
             throw new DITAOTException(e);
         }
 
-        final Collection<FileInfo> files = job.getFileInfo().values();
+        final Collection<FileInfo> files = job.getFileInfo();
         
         final ProfilingFilter writer = new ProfilingFilter();
         writer.setLogger(logger);

src/main/java/org/dita/dost/module/GenMapAndTopicListDebugAndFilterModule.java
@@ -832,14 +832,14 @@ public final class GenMapAndTopicListDebugAndFilterModule implements AbstractPip
         for (final String dst: copytoMap.keySet()) {
             final String src = copytoMap.get(dst);
             //if (new File(baseInputDir + File.separator + prefix, src).exists()) {
-            if (prop.getFileInfo().containsKey(dst)) {
+            if (prop.getFileInfoMap().containsKey(dst)) {
                 tempMap.put(dst, src);
                 // Add the copy-to target to conreflist when its source has
                 // conref
 //                if (conrefSet.contains(src)) {
 //                    conrefSet.add(dst);
 //                }
-                final FileInfo orig = prop.getFileInfo().get(src);
+                final FileInfo orig = prop.getFileInfoMap().get(src);
                 final FileInfo.Builder b = new FileInfo.Builder(orig);
                 b.uri(toURI(new File(dst)));
                 final FileInfo f = b.build();

src/main/java/org/dita/dost/module/ImageMetadataModule.java
@@ -66,7 +66,7 @@ final class ImageMetadataModule implements AbstractPipelineModule {
                                                                        tempDir,
                                                                        job.getProperty("uplevels"));
         writer.setLogger(logger);
-        for (final FileInfo f: job.getFileInfo().values()) {
+        for (final FileInfo f: job.getFileInfo()) {
             if (!f.isResourceOnly && ((f.isActive && "dita".equals(f.format)) || f.isChunked || f.isChunkedDitaMap)) {
                 writer.write(new File(tempDir, f.file).getAbsolutePath());
             }

commit 6ba7d8d4ee6a7eed6fbbb565bfaa127372b91e17

Fix bug in job configuration
9f54639 introduced a bug that could only be reproduced with integration
test 3308775.

src/main/java/org/dita/dost/util/Job.java
@@ -499,7 +499,9 @@ public final class Job {
      * @return collection of file info objects, may be empty
      */
     public Collection<FileInfo> getFileInfo() {
-        return Collections.unmodifiableCollection(files.values());
+        // FIXME: For some reason, integration test 3308775 fails if the implementation is e.g.
+        // return Collections.unmodifiableCollection(new ArrayList<FileInfo>(files.values()));
+        return getFileInfoMap().values();
     }
     
     /**