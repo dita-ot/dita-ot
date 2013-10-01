43d8f35cb39b588e6514341bc6e4947bf853f95a
Jarno Elovirta <jarno@elovirta.com>
2013-10-01 23:49:43 +0300

    Fix integration unit tests

--- src/test/java/org/dita/dost/platform/IntegratorTest.java
+++ src/test/java/org/dita/dost/platform/IntegratorTest.java
@@ -54,7 +54,10 @@ public class IntegratorTest {
         i.setProperties(new File(tempDir, "integrator.properties"));
         i.execute();
 
-        assertEquals(getProperties(new File(expDir, "lib" + File.separator + Integrator.class.getPackage().getName() + File.separator + Constants.GEN_CONF_PROPERTIES)),
+        final Properties expProperties = getProperties(new File(expDir, "lib" + File.separator + Integrator.class.getPackage().getName() + File.separator + Constants.GEN_CONF_PROPERTIES));
+        expProperties.setProperty("dita.plugin.base.dir", new File(tempDir, "plugins" + File.separator + "base").getAbsolutePath());
+        expProperties.setProperty("dita.plugin.dummy.dir", new File(tempDir, "plugins" + File.separator + "dummy").getAbsolutePath());
+        assertEquals(expProperties,
                 getProperties(new File(tempDir, "lib" + File.separator + Integrator.class.getPackage().getName() + File.separator + Constants.GEN_CONF_PROPERTIES)));
         TestUtils.resetXMLUnit();
         XMLUnit.setNormalizeWhitespace(true);
