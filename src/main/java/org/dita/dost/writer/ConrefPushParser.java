20c298211de2b4666fac0a1d0de4de4452d38ebf
Jarno Elovirta <jarno@elovirta.com>
2013-12-01 22:36:48 +0200

    Move conref.list generation to Ant

--- src/main/java/org/dita/dost/writer/ConrefPushParser.java
+++ src/main/java/org/dita/dost/writer/ConrefPushParser.java
@@ -218,41 +218,13 @@ public final class ConrefPushParser extends AbstractXMLWriter {
      * @param filename filename
      */
     private void updateList(final File filename) {
-        // this is used to update the conref.list file.
-        BufferedWriter bufferedWriter =null;
         try {
-            // get the reletivePath from tempDir
             final String reletivePath = filename.getAbsolutePath().substring(FileUtils.normalize(tempDir.toString()).getPath().length() + 1);
-            for (final FileInfo f: job.getFileInfo()) {
-                final String str = f.file.getPath();
-                if (f.hasConref) {
-                    if (str.equals(reletivePath)) {
-                        return;
-                    }
-                }
-            }
             job.getOrCreateFileInfo(reletivePath).hasConref = true;
-            
             job.write();
-
-            try {
-                bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(tempDir, CONREF_LIST_FILE))));
-                for (final FileInfo f: job.getFileInfo()) {
-                    final String str = f.file.getPath();
-                    if (f.hasConref) {
-                        bufferedWriter.append(str).append("\n");
-                    }
-                }
-                bufferedWriter.append(reletivePath);
-            } finally {
-                if (bufferedWriter != null) {
-                    bufferedWriter.close();
-                }
-            }
         } catch (final Exception e) {
             logger.logError(e.getMessage(), e) ;
         }
-
     }
 
     @Override
