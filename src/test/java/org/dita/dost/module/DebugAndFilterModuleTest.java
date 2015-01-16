0a6d8fac5aa700f5f8fe9d80a9faf8d5d3f067fc
Jarno Elovirta <jarno@elovirta.com>
2015-01-16 21:29:21 +0200

    Correct unit tests

--- src/test/java/org/dita/dost/module/DebugAndFilterModuleTest.java
+++ src/test/java/org/dita/dost/module/DebugAndFilterModuleTest.java
@@ -194,7 +194,7 @@ public class DebugAndFilterModuleTest {
             final String xtrf = atts.getValue("xtrf");
             assertNotNull(xtrf);
             //assertEquals(source.getAbsoluteFile().toURI().toString(), xtrf);
-            assertEquals(toURI(source.getAbsoluteFile().getPath()).toString(), xtrf);
+            assertEquals(source.getAbsolutePath(), xtrf);
             final String xtrc = atts.getValue("xtrc");
             assertNotNull(xtrc);
             Integer c = counter.get(localName);
