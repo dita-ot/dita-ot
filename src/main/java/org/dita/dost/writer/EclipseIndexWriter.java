66b4c5cccbf4c3dd09a9e16c518a40a00ea6bfef
Jarno Elovirta <jarno@elovirta.com>
2016-11-14 12:00:01 +0100

    Fix title for eclipse index

--- src/main/java/org/dita/dost/writer/EclipseIndexWriter.java
+++ src/main/java/org/dita/dost/writer/EclipseIndexWriter.java
@@ -218,9 +218,10 @@ public final class EclipseIndexWriter extends AbstractExtendDitaWriter {
                                 foundIndexTerm = true;
                                 if (termClone.getTargetList().size() > 0) {
                                     serializer.writeStartElement("topic");
-                                    serializer.writeAttribute("href", replaceExtName(termClone.getTargetList().get(0).getTargetURI()));
-                                    if (targetName.trim().length() > 0) {
-                                        serializer.writeAttribute("title", termClone.getTargetList().get(0).getTargetName());
+                                    final IndexTermTarget indexTermTarget = termClone.getTargetList().get(0);
+                                    serializer.writeAttribute("href", replaceExtName(indexTermTarget.getTargetURI()));
+                                    if (indexTermTarget.getTargetName() != null && !indexTermTarget.getTargetName().trim().isEmpty()) {
+                                        serializer.writeAttribute("title", indexTermTarget.getTargetName());
                                     }
                                     serializer.writeEndElement(); // topic
                                 }
