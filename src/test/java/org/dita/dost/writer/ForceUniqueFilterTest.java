c3c25ba7c9af9e79c98840b4c01ca099e9487321
Jarno Elovirta <jarno@elovirta.com>
2017-01-03 07:46:01 +0200

    Improve force unique unit test

--- src/test/java/org/dita/dost/writer/ForceUniqueFilterTest.java
+++ src/test/java/org/dita/dost/writer/ForceUniqueFilterTest.java
@@ -7,11 +7,13 @@
  */
 package org.dita.dost.writer;
 
+import com.google.common.collect.ImmutableMap;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.dita.dost.TestUtils;
 import org.dita.dost.module.GenMapAndTopicListModule;
 import org.dita.dost.module.GenMapAndTopicListModule.TempFileNameScheme;
 import org.dita.dost.util.Job;
+import org.dita.dost.util.Job.FileInfo;
 import org.dita.dost.util.Job.FileInfo.Builder;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,9 +27,11 @@ import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.sax.SAXSource;
 import java.io.File;
 import java.net.URI;
+import java.util.HashMap;
 
 import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
 import static org.dita.dost.util.Constants.INPUT_DIR_URI;
+import static org.junit.Assert.assertEquals;
 
 public class ForceUniqueFilterTest {
 
@@ -43,11 +47,30 @@ public class ForceUniqueFilterTest {
         job = new Job(srcDir);
         job.setProperty(INPUT_DIR_URI, srcDir.toURI().toString());
         job.add(new Builder()
+                .src(srcDir.toURI().resolve("test.ditamap"))
+                .uri(URI.create("test.ditamap"))
+                .result(srcDir.toURI().resolve("test.ditamap"))
+                .format("ditamap")
+                .build());
+        job.add(new Builder()
                 .src(srcDir.toURI().resolve("test.dita"))
                 .uri(URI.create("test.dita"))
                 .result(srcDir.toURI().resolve("test.dita"))
                 .build());
-
+        job.add(new Builder()
+                .src(srcDir.toURI().resolve("test3.dita"))
+                .uri(URI.create("test3.dita"))
+                .result(srcDir.toURI().resolve("test3.dita"))
+                .build());
+        job.add(new Builder()
+                .uri(URI.create("copy-to.dita"))
+                .result(srcDir.toURI().resolve("copy-to.dita"))
+                .build());
+        job.add(new Builder()
+                .src(srcDir.toURI().resolve("test2.dita"))
+                .uri(URI.create("test2.dita"))
+                .result(srcDir.toURI().resolve("test2.dita"))
+                .build());
         tempFileNameScheme = new GenMapAndTopicListModule.DefaultTempFileScheme();
         tempFileNameScheme.setBaseDir(srcDir.toURI());
 
@@ -69,6 +92,21 @@ public class ForceUniqueFilterTest {
 
         final Document exp = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new File(expDir, "test.ditamap").toURI().toString()));
         assertXMLEqual(exp, (Document) dst.getNode());
+
+        assertEquals(new HashMap<FileInfo, FileInfo>(ImmutableMap.of(
+                createFileInfo("test.dita", "test_3.dita"),
+                createFileInfo("test.dita", "test.dita"),
+                createFileInfo("test.dita", "test_2.dita"),
+                createFileInfo("test.dita", "test.dita")
+        )), f.copyToMap);
+    }
+
+    private FileInfo createFileInfo(final String src, final String tmp) {
+        return new Builder()
+                .src(srcDir.toURI().resolve(src))
+                .uri(URI.create(tmp))
+                .result(srcDir.toURI().resolve(tmp))
+                .build();
     }
 
 
