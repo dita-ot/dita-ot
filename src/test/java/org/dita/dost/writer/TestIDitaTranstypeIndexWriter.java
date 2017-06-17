a25990fa073f5c61e1505f274887714faee98135
Jarno Elovirta <jarno@elovirta.com>
2017-06-17 09:41:39 +0300

    Remove remaining JavaHelp tests #2726

--- src/test/java/org/dita/dost/writer/TestIDitaTranstypeIndexWriter.java
+++ src/test/java/org/dita/dost/writer/TestIDitaTranstypeIndexWriter.java
@@ -5,31 +5,30 @@
  *
  * See the accompanying LICENSE file for applicable license.
  */
+
 package org.dita.dost.writer;
-import java.io.File;
 
 import org.dita.dost.TestUtils;
-import org.dita.dost.writer.CHMIndexWriter;
-import org.dita.dost.writer.EclipseIndexWriter;
-import org.dita.dost.writer.IDitaTranstypeIndexWriter;
-import org.dita.dost.writer.JavaHelpIndexWriter;
 import org.junit.Test;
+
+import java.io.File;
+
 import static org.junit.Assert.assertEquals;
 
-
 public class TestIDitaTranstypeIndexWriter {
-    public static IDitaTranstypeIndexWriter idita1= new CHMIndexWriter();
-    public static IDitaTranstypeIndexWriter idita2= new EclipseIndexWriter();
-    public static IDitaTranstypeIndexWriter idita3= new JavaHelpIndexWriter();
+
+    public static IDitaTranstypeIndexWriter idita1 = new CHMIndexWriter();
+    public static IDitaTranstypeIndexWriter idita2 = new EclipseIndexWriter();
+
     @Test
-    public void testiditatranstypeindexwriter()
-    {
-        final String rootpath=System.getProperty("user.dir");
-        final String path=rootpath + File.separator + "resources" + File.separator + "index.xml";
-        final String outputfilename="resources" + File.separator + "iditatranstypewriter";
-        assertEquals(TestUtils.testStub.getName() + File.separator + "iditatranstypewriter.hhk",idita1.getIndexFileName(outputfilename));
-        assertEquals(path,idita2.getIndexFileName(outputfilename));
-        assertEquals(TestUtils.testStub.getName() + File.separator + "iditatranstypewriter_index.xml",idita3.getIndexFileName(outputfilename));
+    public void testiditatranstypeindexwriter() {
+        System.err.println(TestUtils.testStub.getName() + File.separator + "iditatranstypewriter_index.xml");
+        final String rootpath = System.getProperty("user.dir");
+        final String path = rootpath + File.separator + "resources" + File.separator + "index.xml";
+        final String outputfilename = "resources" + File.separator + "iditatranstypewriter";
+        assertEquals(TestUtils.testStub.getName() + File.separator + "iditatranstypewriter.hhk", idita1.getIndexFileName(outputfilename));
+        assertEquals(path, idita2.getIndexFileName(outputfilename));
+//        assertEquals(TestUtils.testStub.getName() + File.separator + "iditatranstypewriter_index.xml",idita3.getIndexFileName(outputfilename));
     }
 
 }
