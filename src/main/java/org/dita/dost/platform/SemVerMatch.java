93be09cd3326d71927742b7eafe52897f2fa5e76
Jarno Elovirta <jarno@elovirta.com>
2018-07-30 18:41:29 +0300

    Implement semver as pair of conditions
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- src/main/java/org/dita/dost/platform/SemVerMatch.java
+++ src/main/java/org/dita/dost/platform/SemVerMatch.java
@@ -8,37 +8,130 @@
 
 package org.dita.dost.platform;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class SemVerMatch implements Comparable<SemVerMatch> {
+public class SemVerMatch {
 
     enum Match {
         TILDE,
         CARET,
+        EQ,
         LT,
         LE,
         GT,
         GE
     }
 
+    public static class Range {
+        public final Match match;
+        public final Integer major;
+        public final Integer minor;
+        public final Integer patch;
+
+        public Range(Match match, Integer major, Integer minor, Integer patch) {
+            if (match == null || major == null || minor == null || patch == null) {
+                throw new NullPointerException();
+            }
+            this.match = match;
+            this.major = major;
+            this.minor = minor;
+            this.patch = patch;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder buf = new StringBuilder();
+            if (match != null) {
+                switch (match) {
+                    case TILDE:
+                        buf.append("~");
+                        break;
+                    case CARET:
+                        buf.append("^");
+                        break;
+                    case LT:
+                        buf.append("<");
+                        break;
+                    case LE:
+                        buf.append("<=");
+                        break;
+                    case GT:
+                        buf.append(">");
+                        break;
+                    case GE:
+                        buf.append(">=");
+                        break;
+                    case EQ:
+                        break;
+                    default:
+                        throw new IllegalArgumentException();
+                }
+            }
+            buf.append(major.toString())
+                    .append('.')
+                    .append(minor != null ? minor.toString() : "x")
+                    .append('.')
+                    .append(patch != null ? patch.toString() : "x");
+            return buf.toString();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Range range = (Range) o;
+            return match == range.match &&
+                    Objects.equals(major, range.major) &&
+                    Objects.equals(minor, range.minor) &&
+                    Objects.equals(patch, range.patch);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(match, major, minor, patch);
+        }
+    }
+
+
     private static final Pattern MATCH_PATTERN = Pattern.compile("(~|\\^|<=?|>=?)?(.+)");
 
-    public final Match match;
-    public final Integer major;
-    public final Integer minor;
-    public final Integer patch;
+    public final Range start;
+    public final Range end;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SemVerMatch that = (SemVerMatch) o;
+        return Objects.equals(start, that.start) &&
+                Objects.equals(end, that.end);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end);
+    }
+
+    public SemVerMatch(Range start, Range end) {
+        this.start = start;
+        this.end = end;
+    }
 
     public SemVerMatch(String str) {
+        if (str.equals("") || str.equals("*")) {
+            start = new Range(Match.GE, 0, 0, 0);
+            end = null;
+            return;
+        }
         final Matcher matcher = MATCH_PATTERN.matcher(str);
         if (!matcher.matches()) {
             throw new IllegalArgumentException();
         }
-        String[] tokens = matcher.group(2).split("\\.");
-        major = Integer.valueOf(tokens[0]);
-        minor = tokens.length >= 2 ? Integer.valueOf(tokens[1]) : null;
-        patch = tokens.length >= 3 ? Integer.valueOf(tokens[2]) : null;
+        final Match match;
         if (matcher.group(1) != null) {
             switch (matcher.group(1)) {
                 case "~":
@@ -63,48 +156,137 @@ public class SemVerMatch implements Comparable<SemVerMatch> {
                     throw new IllegalArgumentException();
             }
         } else {
-            match = null;
+            match = Match.EQ;
+        }
+        String[] tokens = matcher.group(2).split("\\.");
+        final Integer major = tokens.length >= 1 ? parseToken(tokens[0]) : null;
+        final Integer minor = tokens.length >= 2 ? parseToken(tokens[1]) : null;
+        final Integer patch = tokens.length >= 3 ? parseToken(tokens[2]) : null;
+
+        switch (match) {
+            case TILDE:
+                start = new Range(Match.GE,
+                        major != null ? major : 0,
+                        minor != null ? minor : 0,
+                        patch != null ? patch : 0);
+                end = tilde(Match.LT, major, minor);
+                break;
+            case CARET:
+                start = new Range(Match.GE,
+                        major != null ? major : 0,
+                        minor != null ? minor : 0,
+                        patch != null ? patch : 0);
+                end = caret(Match.LT, major, minor, patch);
+                break;
+            case LT:
+                start = new Range(Match.GE, 0, 0, 0);
+                end = new Range(Match.LT,
+                        major != null ? major : 0,
+                        minor != null ? minor : 0,
+                        patch != null ? patch : 0);
+                break;
+            case LE:
+                start = new Range(Match.GE, 0, 0, 0);
+                end = new Range(Match.LE,
+                        major != null ? major : 0,
+                        minor != null ? minor : 0,
+                        patch != null ? patch : 0);
+                break;
+            case GT:
+                start = new Range(Match.GT,
+                        major != null ? major : 0,
+                        minor != null ? minor : 0,
+                        patch != null ? patch : 0);
+                end = null;
+                break;
+            case GE:
+                start = new Range(Match.GE,
+                        major != null ? major : 0,
+                        minor != null ? minor : 0,
+                        patch != null ? patch : 0);
+                end = null;
+                break;
+            case EQ:
+                start = new Range(Match.GE,
+                        major != null ? major : 0,
+                        minor != null ? minor : 0,
+                        patch != null ? patch : 0);
+                end = inc(Match.LT, major, minor, patch);
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    private static Range inc(Match match, Integer major, Integer minor, Integer patch) {
+        final Integer major1 = major != null && minor == null && patch == null ? Integer.valueOf(major + 1) : major;
+        final Integer minor1 = null != minor && patch == null ? Integer.valueOf(minor + 1) : minor;
+        final Integer patch1 = patch != null ? Integer.valueOf(patch + 1) : patch;
+        return new Range(match,
+                major1 != null ? major1 : 0,
+                minor1 != null ? minor1 : 0,
+                patch1 != null ? patch1 : 0);
+    }
+
+    private static Range tilde(Match match, Integer major, Integer minor) {
+        final Integer major1 = major != null && minor == null ? Integer.valueOf(major + 1) : major;
+        final Integer minor1 = minor != null ? Integer.valueOf(minor + 1) : null;
+        final Integer patch1 = null;
+        return new Range(match,
+                major1 != null ? major1 : 0,
+                minor1 != null ? minor1 : 0,
+                patch1 != null ? patch1 : 0);
+    }
+
+    private static Range caret(Match match, Integer major, Integer minor, Integer patch) {
+        final List<Integer> tokens = new ArrayList<>();
+        if (major != null) {
+            tokens.add(major);
+            if (minor != null) {
+                tokens.add(minor);
+                if (patch != null) {
+                    tokens.add(patch);
+                }
+            }
+        }
+        // Find first non-null
+        int i = 0;
+        for (; i < tokens.size(); i++) {
+            if (tokens.get(i) != 0) {
+                break;
+            }
+        }
+        if (i == tokens.size()) {
+            i = tokens.size() - 1;
+        }
+
+        // increment first non-null
+        tokens.set(i, tokens.get(i) + 1);
+        // zero after first non-null
+        for (i++; i < tokens.size(); i++) {
+            tokens.set(i, 0);
+        }
+
+        return new Range(match,
+                tokens.size() > 0 && tokens.get(0) != null ? tokens.get(0) : 0,
+                tokens.size() > 1 && tokens.get(1) != null ? tokens.get(1) : 0,
+                tokens.size() > 2 && tokens.get(2) != null ? tokens.get(2) : 0);
+    }
+
+    private static Integer parseToken(String token) {
+        switch (token) {
+            case "x":
+            case "X":
+            case "*":
+                return null;
+            default:
+                return Integer.valueOf(token);
         }
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SemVerMatch semVer = (SemVerMatch) o;
-        return major == semVer.major &&
-                minor == semVer.minor &&
-                patch == semVer.patch;
+    public String toString() {
+        return start + (start != null && end != null ? " " : "") + end;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(major, minor, patch);
-    }
-
-    @Override
-    public int compareTo(SemVerMatch o) {
-        if (o.major == this.major && o.minor == this.minor && o.patch == this.patch) {
-            return 0;
-        }
-        if (o.major > this.major) {
-            return -1;
-        }
-        if (o.major < this.major) {
-            return 1;
-        }
-        if (o.minor > this.minor) {
-            return -1;
-        }
-        if (o.minor < this.minor) {
-            return 1;
-        }
-        if (o.patch > this.patch) {
-            return -1;
-        }
-        if (o.patch < this.patch) {
-            return 1;
-        }
-        throw new IllegalStateException();
-    }
 }
--- src/test/java/org/dita/dost/platform/SemVerMatchTest.java
+++ src/test/java/org/dita/dost/platform/SemVerMatchTest.java
@@ -10,26 +10,201 @@ package org.dita.dost.platform;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class SemVerMatchTest {
 
     @Test
     public void testPlainConstructor() {
-        final SemVerMatch m = new SemVerMatch("1.2.3");
-        assertEquals(null, m.match);
-        assertEquals(Integer.valueOf(1), m.major);
-        assertEquals(Integer.valueOf(2), m.minor);
-        assertEquals(Integer.valueOf(3), m.patch);
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 1, 2, 3),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 1, 2, 4)
+                ),
+                new SemVerMatch("1.2.3"));
+    }
+
+    @Test
+    public void testXRangeConstructor() {
+        //    * := >=0.0.0 (Any version satisfies)
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 0, 0, 0),
+                        null
+                ),
+                new SemVerMatch("*"));
+        //        1.x := >=1.0.0 <2.0.0 (Matching major version)
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 1, 0, 0),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 2, 0, 0)
+                ),
+                new SemVerMatch("1.x"));
+        //        1.2.x := >=1.2.0 <1.3.0 (Matching major and minor versions)
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 1, 2, 0),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 1, 3, 0)
+                ),
+                new SemVerMatch("1.2.x"));
+        //        "" (empty string) := * := >=0.0.0
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 0, 0, 0),
+                        null
+                ),
+                new SemVerMatch("*"));
+        //        1 := 1.x.x := >=1.0.0 <2.0.0
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 1, 0, 0),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 2, 0, 0)
+                ),
+                new SemVerMatch("1"));
+        //        1.2 := 1.2.x := >=1.2.0 <1.3.0
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 1, 2, 0),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 1, 3, 0)
+                ),
+                new SemVerMatch("1.2"));
     }
 
     @Test
     public void testTildeConstructor() {
-        final SemVerMatch m = new SemVerMatch("~1.2");
-        assertEquals(SemVerMatch.Match.TILDE, m.match);
-        assertEquals(Integer.valueOf(1), m.major);
-        assertEquals(Integer.valueOf(2), m.minor);
-        assertEquals(null, m.patch);
+        //        ~1.2.3 := >=1.2.3 <1.(2+1).0 := >=1.2.3 <1.3.0
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 1, 2, 3),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 1, 3, 0)
+                ),
+                new SemVerMatch("~1.2.3"));
+        //        ~1.2 := >=1.2.0 <1.(2+1).0 := >=1.2.0 <1.3.0 (Same as 1.2.x)
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 1, 2, 0),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 1, 3, 0)
+                ),
+                new SemVerMatch("~1.2"));
+        //        ~1 := >=1.0.0 <(1+1).0.0 := >=1.0.0 <2.0.0 (Same as 1.x)
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 1, 0, 0),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 2, 0, 0)
+                ),
+                new SemVerMatch("~1"));
+        //        ~0.2.3 := >=0.2.3 <0.(2+1).0 := >=0.2.3 <0.3.0
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 0, 2, 3),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 0, 3, 0)
+                ),
+                new SemVerMatch("~0.2.3"));
+        //        ~0.2 := >=0.2.0 <0.(2+1).0 := >=0.2.0 <0.3.0 (Same as 0.2.x)
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 0, 2, 0),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 0, 3, 0)
+                ),
+                new SemVerMatch("~0.2"));
+        //        ~0 := >=0.0.0 <(0+1).0.0 := >=0.0.0 <1.0.0 (Same as 0.x)
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 0, 0, 0),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 1, 0, 0)
+                ),
+                new SemVerMatch("~0"));
+        //        ~1.2.3-beta.2 := >=1.2.3-beta.2 <1.3.0
+//        assertEquals(
+//                new SemVerMatch(
+//                        new SemVerMatch.Range(SemVerMatch.Match.GE, 1, 2, 3),
+//                        new SemVerMatch.Range(SemVerMatch.Match.LT, 1, 3, 0)
+//                ),
+//                new SemVerMatch("~1.2.3-beta.2"));
+    }
+
+    @Test
+    public void testCaretConstructor() {
+        //        ^1.2.3 := >=1.2.3 <2.0.0
+        final SemVerMatch actual1 = new SemVerMatch("^1.2.3");
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 1, 2, 3),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 2, 0, 0)
+                ),
+                actual1);
+        //        ^0.2.3 := >=0.2.3 <0.3.0
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 0, 2, 3),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 0, 3, 0)
+                ),
+                new SemVerMatch("^0.2.3"));
+        //        ^0.0.3 := >=0.0.3 <0.0.4
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 0, 0, 3),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 0, 0, 4)
+                ),
+                new SemVerMatch("^0.0.3"));
+        //        ^1.2.3-beta.2 := >=1.2.3-beta.2 <2.0.0
+//        assertEquals(
+//                new SemVerMatch(
+//                        new SemVerMatch.Range(SemVerMatch.Match.GE, 1, 2, 3),
+//                        new SemVerMatch.Range(SemVerMatch.Match.LT, 2, 0, 0)
+//                ),
+//                new SemVerMatch("1.2.3-beta.2"));
+        //        ^0.0.3-beta := >=0.0.3-beta <0.0.4
+//        assertEquals(
+//                new SemVerMatch(
+//                        new SemVerMatch.Range(SemVerMatch.Match.GE, 0, 0, 3),
+//                        new SemVerMatch.Range(SemVerMatch.Match.LT, 0, 0, 4)
+//                ),
+//                new SemVerMatch("^0.0.3-beta"));
+        //        ^1.2.x := >=1.2.0 <2.0.0
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 1, 2, 0),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 2, 0, 0)
+                ),
+                new SemVerMatch("^1.2.x"));
+        //        ^0.0.x := >=0.0.0 <0.1.0
+        final SemVerMatch actual = new SemVerMatch("^0.0.x");
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 0, 0, 0),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 0, 1, 0)
+                ),
+                actual);
+        //        ^0.0 := >=0.0.0 <0.1.0
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 0, 0, 0),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 0, 1, 0)
+                ),
+                new SemVerMatch("^0.0"));
+        //        ^1.x := >=1.0.0 <2.0.0
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 1, 0, 0),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 2, 0, 0)
+                ),
+                new SemVerMatch("^1.x"));
+        //        ^0.x := >=0.0.0 <1.0.0
+        assertEquals(
+                new SemVerMatch(
+                        new SemVerMatch.Range(SemVerMatch.Match.GE, 0, 0, 0),
+                        new SemVerMatch.Range(SemVerMatch.Match.LT, 1, 0, 0)
+                ),
+                new SemVerMatch("^0.x"));
+    }
+
+    @Test
+    public void testHyphenRangeParse() {
+        //        1.2.3 - 2.3.4 := >=1.2.3 <=2.3.4
+        //        1.2 - 2.3.4 := >=1.2.0 <=2.3.4
+        //        1.2.3 - 2.3 := >=1.2.3 <2.4.0
+        //        1.2.3 - 2 := >=1.2.3 <3.0.0
     }
 
 }
