67d069aa4ddcb60d85c3e476d6fff9885a99eb3f
Jarno Elovirta <jarno@elovirta.com>
2020-09-08 18:14:13 +0300

    Fix indentation and remove change log comments
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- src/main/java/org/dita/dost/util/XMLGrammarPoolImplUtils.java
+++ src/main/java/org/dita/dost/util/XMLGrammarPoolImplUtils.java
@@ -68,12 +68,11 @@ public final class XMLGrammarPoolImplUtils extends XMLGrammarPoolImpl {
             // return -1 for XSD grammar hashcode because we want to disable XSD grammar caching
             return -1;
         } else {
-        	//#3568 Prefer to hash the public ID if available
-        	if(desc.getPublicId() != null) {
-        		return desc.getPublicId().hashCode();
-        	} else {
-        		return desc.hashCode();
-        	}
+            if (desc.getPublicId() != null) {
+                return desc.getPublicId().hashCode();
+            } else {
+                return desc.hashCode();
+            }
         }
     }
 
@@ -99,12 +98,11 @@ public final class XMLGrammarPoolImplUtils extends XMLGrammarPoolImpl {
             // always return false for XSD grammar to disable XSD grammar caching
             return false;
         } else {
-        	//#3568 Compare descriptions only by public ID if available.
-        	if(desc1.getPublicId() != null && desc2.getPublicId() != null) {
-        		return desc1.getPublicId().equals(desc2.getPublicId());
-        	} else {
-        		return desc1.equals(desc2);
-        	}
+            if (desc1.getPublicId() != null && desc2.getPublicId() != null) {
+                return desc1.getPublicId().equals(desc2.getPublicId());
+            } else {
+                return desc1.equals(desc2);
+            }
         }
     }
 
