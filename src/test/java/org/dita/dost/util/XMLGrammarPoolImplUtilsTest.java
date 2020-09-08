6abe7e48447758f1208c04ec58c78829e25a810a
Jarno Elovirta <jarno@elovirta.com>
2020-09-08 19:36:30 +0300

    Fix indentation
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- src/test/java/org/dita/dost/util/XMLGrammarPoolImplUtilsTest.java
+++ src/test/java/org/dita/dost/util/XMLGrammarPoolImplUtilsTest.java
@@ -7,77 +7,75 @@
  */
 package org.dita.dost.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-
-import java.io.File;
-
 import org.apache.xerces.impl.dtd.DTDGrammar;
 import org.apache.xerces.impl.dtd.XMLDTDDescription;
 import org.apache.xerces.xni.grammars.Grammar;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 
 public class XMLGrammarPoolImplUtilsTest {
 
-	private XMLGrammarPoolImplUtils utils;
+    private XMLGrammarPoolImplUtils utils;
 
-	@Before
-	public void setUp() {
-		utils = new XMLGrammarPoolImplUtils();
-	}
+    @Before
+    public void setUp() {
+        utils = new XMLGrammarPoolImplUtils();
+    }
 
-	@Test
-	public void testCompareDescriptorsByPublicID_same() {
-		final XMLDTDDescription desc1 = getXmldtdDescription("file:/foo/abc.xml", "file:/foo/abc.dtd", "publicID");
-		final DTDGrammar exp = new DTDGrammar(null, desc1);
-		utils.putGrammar(exp);
+    @Test
+    public void testCompareDescriptorsByPublicID_same() {
+        final XMLDTDDescription desc1 = getXmldtdDescription("file:/foo/abc.xml", "file:/foo/abc.dtd", "publicID");
+        final DTDGrammar exp = new DTDGrammar(null, desc1);
+        utils.putGrammar(exp);
 
-		final XMLDTDDescription desc2 = getXmldtdDescription("file:/abc.xml", "file:/abc.dtd", "publicID");
-		final Grammar act = utils.getGrammar(desc2);
+        final XMLDTDDescription desc2 = getXmldtdDescription("file:/abc.xml", "file:/abc.dtd", "publicID");
+        final Grammar act = utils.getGrammar(desc2);
 
-		assertEquals(exp, act);
-	}
+        assertEquals(exp, act);
+    }
 
-	@Test
+    @Test
     public void testCompareDescriptorsByPublicID_different() {
-		final XMLDTDDescription desc1 = getXmldtdDescription("file:/foo/abc.xml", "file:/foo/abc.dtd", "publicID");
-		final DTDGrammar exp = new DTDGrammar(null, desc1);
-    	utils.putGrammar(exp);
+        final XMLDTDDescription desc1 = getXmldtdDescription("file:/foo/abc.xml", "file:/foo/abc.dtd", "publicID");
+        final DTDGrammar exp = new DTDGrammar(null, desc1);
+        utils.putGrammar(exp);
 
-		final XMLDTDDescription desc2 = getXmldtdDescription("file:/abc.xml", "file:/abc.dtd", "differentId");
-		final Grammar act = utils.getGrammar(desc2);
+        final XMLDTDDescription desc2 = getXmldtdDescription("file:/abc.xml", "file:/abc.dtd", "differentId");
+        final Grammar act = utils.getGrammar(desc2);
 
-		assertNull(act);
+        assertNull(act);
     }
 
-	@Test
-	public void testCompareDescriptorsBySystemID_same() {
-		final XMLDTDDescription desc1 = getXmldtdDescription("file:/foo/abc.xml", "file:/foo/abc.dtd", null);
-		final DTDGrammar exp = new DTDGrammar(null, desc1);
-		utils.putGrammar(exp);
+    @Test
+    public void testCompareDescriptorsBySystemID_same() {
+        final XMLDTDDescription desc1 = getXmldtdDescription("file:/foo/abc.xml", "file:/foo/abc.dtd", null);
+        final DTDGrammar exp = new DTDGrammar(null, desc1);
+        utils.putGrammar(exp);
 
-		final XMLDTDDescription desc2 = getXmldtdDescription("file:/abc.xml", "file:/foo/abc.dtd", null);
-		final Grammar act = utils.getGrammar(desc2);
+        final XMLDTDDescription desc2 = getXmldtdDescription("file:/abc.xml", "file:/foo/abc.dtd", null);
+        final Grammar act = utils.getGrammar(desc2);
 
-		assertEquals(exp, act);
-	}
+        assertEquals(exp, act);
+    }
 
-	@Test
+    @Test
     public void testCompareDescriptorsBySystemID_different() {
-		final XMLDTDDescription desc1 = getXmldtdDescription("file:/foo/abc.xml", "file:/foo/abc.dtd", null);
-		final DTDGrammar exp = new DTDGrammar(null, desc1);
-    	utils.putGrammar(exp);
+        final XMLDTDDescription desc1 = getXmldtdDescription("file:/foo/abc.xml", "file:/foo/abc.dtd", null);
+        final DTDGrammar exp = new DTDGrammar(null, desc1);
+        utils.putGrammar(exp);
 
-		final XMLDTDDescription desc2 = getXmldtdDescription("file:/abc.xml", "file:/abc.dtd", null);
-		final Grammar act = utils.getGrammar(desc2);
+        final XMLDTDDescription desc2 = getXmldtdDescription("file:/abc.xml", "file:/abc.dtd", null);
+        final Grammar act = utils.getGrammar(desc2);
 
-    	assertNull(act);
+        assertNull(act);
     }
 
-	private XMLDTDDescription getXmldtdDescription(String base, String systemId, String publicId) {
-		return new XMLDTDDescription(publicId, "topic.dtd", base, systemId, "topic");
-	}
+    private XMLDTDDescription getXmldtdDescription(String base, String systemId, String publicId) {
+        return new XMLDTDDescription(publicId, "topic.dtd", base, systemId, "topic");
+    }
 
 }
