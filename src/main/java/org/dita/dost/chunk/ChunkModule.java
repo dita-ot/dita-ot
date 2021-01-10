19831f6ae7e44690a7c1b6e8b8ea391def2bda88
Jarno Elovirta <jarno@elovirta.com>
2021-01-10 16:24:13 +0200

    Initial chunk module for combine action
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

---
+++ src/main/java/org/dita/dost/chunk/ChunkModule.java
@@ -0,0 +1,21 @@
+/*
+ * This file is part of the DITA Open Toolkit project.
+ *
+ * Copyright 2021 Jarno Elovirta
+ *
+ * See the accompanying LICENSE file for applicable license.
+ */
+
+package org.dita.dost.chunk;
+
+import org.dita.dost.exception.DITAOTException;
+import org.dita.dost.module.AbstractPipelineModuleImpl;
+import org.dita.dost.pipeline.AbstractPipelineInput;
+import org.dita.dost.pipeline.AbstractPipelineOutput;
+
+public class ChunkModule extends AbstractPipelineModuleImpl {
+    @Override
+    public AbstractPipelineOutput execute(final AbstractPipelineInput input) throws DITAOTException {
+
+    }
+}
