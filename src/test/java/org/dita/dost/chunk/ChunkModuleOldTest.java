3d753fe3c023051a32007ce516f45c3fe66cfc4d
Jarno Elovirta <jarno@elovirta.com>
2021-01-24 10:45:44 +0200

    Add matching tests for old chunk implementation
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

---
+++ src/test/java/org/dita/dost/chunk/ChunkModuleOldTest.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the DITA Open Toolkit project.
+ *
+ * Copyright 2021 Jarno Elovirta
+ *
+ * See the accompanying LICENSE file for applicable license.
+ */
+
+package org.dita.dost.chunk;
+
+import org.dita.dost.module.ChunkModule;
+import org.dita.dost.module.AbstractModuleTest;
+import org.dita.dost.module.AbstractPipelineModule;
+import org.dita.dost.pipeline.AbstractPipelineInput;
+import org.dita.dost.pipeline.PipelineHashIO;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.dita.dost.util.Constants.ANT_INVOKER_EXT_PARAM_TRANSTYPE;
+
+public class ChunkModuleOldTest extends AbstractModuleTest {
+    @Override
+    protected AbstractPipelineInput getAbstractPipelineInput() {
+        final AbstractPipelineInput input = new PipelineHashIO();
+        input.setAttribute(ANT_INVOKER_EXT_PARAM_TRANSTYPE, "html5");
+        return input;
+    }
+    @Override
+    protected AbstractPipelineModule getModule(final File tempDir) {
+        return new ChunkModule();
+    }
+
+    @Test
+    public void combine() {
+        test("combine.ditamap");
+    }
+
+    @Test
+    public void link() {
+        test("link.ditamap");
+    }
+}
