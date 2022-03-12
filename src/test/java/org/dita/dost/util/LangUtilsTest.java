32a84d751f3490b8630bcc8f38d49d8a2ddbca20
Jarno Elovirta <jarno@elovirta.com>
2022-03-12 09:31:08 +0200

    Add utility functions
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

---
+++ src/test/java/org/dita/dost/util/LangUtilsTest.java
@@ -0,0 +1,62 @@
+/*
+ * This file is part of the DITA Open Toolkit project.
+ *
+ * Copyright 2022 Jarno Elovirta
+ *
+ * See the accompanying LICENSE file for applicable license.
+ */
+
+package org.dita.dost.util;
+
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.*;
+
+public class LangUtilsTest {
+
+    @Test
+    public void pair() {
+        final Map.Entry<String, String> act = LangUtils.pair("left", "right");
+        assertEquals("left", act.getKey());
+        assertEquals("right", act.getValue());
+    }
+
+    @Test
+    public void pair_null() {
+        final Map.Entry<String, String> act = LangUtils.pair(null, null);
+        assertEquals(null, act.getKey());
+        assertEquals(null, act.getValue());
+    }
+
+    @Test
+    public void zipWithIndex() {
+        final List<String> src = asList("first", "second");
+        final List<Map.Entry<String, Integer>> act = LangUtils.zipWithIndex(src).collect(toList());
+        assertEquals("first", act.get(0).getKey());
+        assertEquals(0, act.get(0).getValue().intValue());
+        assertEquals("second", act.get(1).getKey());
+        assertEquals(1, act.get(1).getValue().intValue());
+    }
+
+    @Test
+    public void zipWithIndex_null() {
+        final List<String> src = new ArrayList<>();
+        src.add(null);
+        final List<Map.Entry<String, Integer>> act = LangUtils.zipWithIndex(src).collect(toList());
+        assertEquals(null, act.get(0).getKey());
+        assertEquals(0, act.get(0).getValue().intValue());
+    }
+
+    @Test
+    public void zipWithIndex_empty() {
+        final List<String> src = Collections.emptyList();
+        final List<Map.Entry<String, Integer>> act = LangUtils.zipWithIndex(src).collect(toList());
+        assertTrue(act.isEmpty());
+    }
+}
