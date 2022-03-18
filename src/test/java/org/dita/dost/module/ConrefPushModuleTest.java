980d4a052284c3f3ae5e4766c5f4347795708497
Jarno Elovirta <jarno@elovirta.com>
2022-03-18 16:45:54 +0200

    Move conref push integration tests to unit tests
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

---
+++ src/test/java/org/dita/dost/module/ConrefPushModuleTest.java
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the DITA Open Toolkit project.
+ *
+ * Copyright 2022 Jarno Elovirta
+ *
+ * See the accompanying LICENSE file for applicable license.
+ */
+
+package org.dita.dost.module;
+
+import org.dita.dost.pipeline.AbstractPipelineInput;
+import org.dita.dost.pipeline.PipelineHashIO;
+import org.dita.dost.util.Configuration.Mode;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.dita.dost.util.Constants.ATTR_FORMAT_VALUE_DITA;
+import static org.dita.dost.util.Constants.ATTR_FORMAT_VALUE_DITAMAP;
+
+@RunWith(Parameterized.class)
+public class ConrefPushModuleTest extends AbstractModuleTest {
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"conref_push", Mode.STRICT},
+                {"pushAfter_between_Specialization", Mode.STRICT},
+                {"pushAfter_with_crossRef", Mode.STRICT},
+                {"pushAfter_with_InvalidTarget", Mode.STRICT},
+                {"pushAfter_without_conref", Mode.STRICT},
+                {"pushBefore_between_Specialization", Mode.STRICT},
+                {"pushBefore_with_crossRef", Mode.STRICT},
+                {"pushBefore_with_InvalidTarget", Mode.LAX},
+                {"pushBefore_without_conref", Mode.STRICT},
+                {"pushReplace_between_Specialization", Mode.STRICT},
+                {"pushReplace_with_crossRef", Mode.STRICT},
+                {"pushReplace_with_InvalidTarget", Mode.LAX},
+                {"pushReplace_without_conref", Mode.STRICT},
+                {"simple_pushAfter", Mode.STRICT},
+                {"simple_pushBefore", Mode.STRICT},
+                {"simple_pushReplace", Mode.STRICT}
+        });
+    }
+
+    public ConrefPushModuleTest(String testCase, Mode mode) {
+        super(testCase, Collections.emptyMap());
+        this.mode = mode;
+    }
+
+    @Override
+    protected AbstractPipelineInput getAbstractPipelineInput() {
+        return new PipelineHashIO();
+    }
+
+    @Override
+    protected AbstractPipelineModule getModule(final File tempDir) {
+        final ConrefPushModule conrefPushModule = new ConrefPushModule();
+        conrefPushModule.setFileInfoFilter(fileInfo ->
+                fileInfo.format.equals(ATTR_FORMAT_VALUE_DITA) ||
+                        fileInfo.format.equals(ATTR_FORMAT_VALUE_DITAMAP) && fileInfo.isInput
+        );
+        return conrefPushModule;
+    }
+}
