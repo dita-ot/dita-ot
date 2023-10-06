efad05cb3fd343779d92c2a8a5d74b8342c99e3d
Jarno Elovirta <jarno@elovirta.com>
2023-10-06 16:00:20 +0300

    Add initial subject scheme reader test
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

---
+++ src/test/java/org/dita/dost/reader/SubjectSchemeReaderTest.java
@@ -0,0 +1,148 @@
+/*
+ * This file is part of the DITA Open Toolkit project.
+ *
+ * Copyright 2023 Jarno Elovirta
+ *
+ * See the accompanying LICENSE file for applicable license.
+ */
+
+package org.dita.dost.reader;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Set;
+import javax.xml.namespace.QName;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import net.sf.saxon.dom.NodeOverNodeInfo;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.sapling.SaplingElement;
+import net.sf.saxon.sapling.Saplings;
+import org.dita.dost.TestUtils;
+import org.dita.dost.module.filter.SubjectScheme;
+import org.dita.dost.store.StreamStore;
+import org.dita.dost.util.Job;
+import org.dita.dost.util.XMLUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xmlunit.builder.DiffBuilder;
+
+class SubjectSchemeReaderTest {
+
+  private final XMLUtils xmlUtils = new XMLUtils();
+
+  private Job job;
+  private SubjectSchemeReader reader;
+
+  @TempDir
+  File tempDir;
+
+  @BeforeEach
+  void init() throws IOException {
+    job = new Job(tempDir, new StreamStore(tempDir, xmlUtils));
+    reader = new SubjectSchemeReader();
+    reader.setJob(job);
+    var logger = new TestUtils.CachingLogger();
+    reader.setLogger(logger);
+  }
+
+  @Test
+  void loadSubjectScheme() throws URISyntaxException, IOException {
+    final Path src = tempDir.toPath().resolve("example-subjectScheme-filtering.ditamap");
+    Files.copy(
+      Paths.get(
+        getClass()
+          .getResource("/org/dita/dost/reader.SubjectSchemeReaderTest.src/example-subjectScheme-filtering.ditamap")
+          .toURI()
+      ),
+      src
+    );
+
+    reader.loadSubjectScheme(src.toFile());
+
+    var act = reader.getSubjectSchemeMap();
+    assertFalse(act.isEmpty());
+    var exp = new SubjectScheme(
+      Map.of(
+        QName.valueOf("platform"),
+        Map.of(
+          "*",
+          Set.of(
+            createElement(
+              createSubjectDef("os", "Operating system")
+                .withChild(
+                  createSubjectDef("linux", "Linux")
+                    .withChild(createSubjectDef("redhat", "RedHat Linux"), createSubjectDef("suse", "SuSE Linux")),
+                  createSubjectDef("windows", "Windows"),
+                  createSubjectDef("zos", "z/OS")
+                )
+            )
+          )
+        )
+      )
+    );
+    extracted(act, exp);
+    assertEquals(
+      Map.of(QName.valueOf("platform"), Map.of("*", Set.of("suse", "linux", "windows", "redhat", "zos"))),
+      reader.getValidValuesMap()
+    );
+    assertEquals(Map.of(), reader.getDefaultValueMap());
+  }
+
+  private void extracted(SubjectScheme act, SubjectScheme exp) {
+    assertEquals(exp.subjectSchemeMap().keySet(), act.subjectSchemeMap().keySet());
+    for (QName expKey : exp.subjectSchemeMap().keySet()) {
+      final Map<String, Set<Element>> actStringSetMap = act.subjectSchemeMap().get(expKey);
+      final Map<String, Set<Element>> expStringSetMap = exp.subjectSchemeMap().get(expKey);
+      assertEquals(expStringSetMap.keySet(), actStringSetMap.keySet());
+      for (String expSetKey : expStringSetMap.keySet()) {
+        var expValueSet = expStringSetMap.get(expSetKey);
+        var actValueSet = actStringSetMap.get(expSetKey);
+        assertEquals(expValueSet.size(), actValueSet.size());
+        assertDocumentEquals(expValueSet.stream().findFirst().get(), actValueSet.stream().findFirst().get());
+      }
+    }
+  }
+
+  private SaplingElement createSubjectDef(String keys, String navTitle) {
+    return Saplings
+      .elem("subjectdef")
+      .withAttr("class", "- map/topicref subjectScheme/subjectdef ")
+      .withAttr("keys", keys)
+      .withAttr("navtitle", navTitle);
+  }
+
+  private void assertDocumentEquals(Element exp, Element act) {
+    var d = DiffBuilder.compare(exp).withTest(act).ignoreWhitespace().build();
+    if (d.hasDifferences()) {
+      try {
+        TransformerFactory.newInstance().newTransformer().transform(new DOMSource(act), new StreamResult(System.out));
+      } catch (TransformerException e) {
+        throw new RuntimeException(e);
+      }
+      throw new AssertionError(d.toString());
+    }
+  }
+
+  private Element createElement(SaplingElement exp) {
+    try {
+      var underlyingValue = Saplings.doc().withChild(exp).toXdmNode(xmlUtils.getProcessor()).getUnderlyingValue();
+      return ((Document) NodeOverNodeInfo.wrap(underlyingValue)).getDocumentElement();
+    } catch (SaxonApiException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
