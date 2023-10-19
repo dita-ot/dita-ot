e7e0e3650daf81aaf4a9d720b249ff829ca066ff
Jarno Elovirta <jarno@elovirta.com>
2023-10-19 20:19:25 +0300

    Disable unused code from preprocess2 old profiling
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- src/main/java/org/dita/dost/module/reader/MapReaderModule.java
+++ src/main/java/org/dita/dost/module/reader/MapReaderModule.java
@@ -8,21 +8,19 @@
 
 package org.dita.dost.module.reader;
 
-import static org.dita.dost.reader.GenListModuleReader.ROOT_URI;
 import static org.dita.dost.util.Constants.*;
 import static org.dita.dost.util.URLUtils.exists;
 
-import java.io.File;
-import java.io.IOException;
 import java.net.URI;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import org.dita.dost.exception.DITAOTException;
 import org.dita.dost.exception.DITAOTXMLErrorHandler;
 import org.dita.dost.log.MessageUtils;
 import org.dita.dost.pipeline.AbstractPipelineInput;
 import org.dita.dost.pipeline.AbstractPipelineOutput;
 import org.dita.dost.reader.GenListModuleReader.Reference;
-import org.dita.dost.reader.SubjectSchemeReader;
 import org.dita.dost.writer.DebugFilter;
 import org.dita.dost.writer.NormalizeFilter;
 import org.dita.dost.writer.ValidationFilter;
@@ -60,18 +58,6 @@ public final class MapReaderModule extends AbstractReaderModule {
       throw new DITAOTException(e.getMessage(), e);
     }
 
-    //    if (profilingEnabled) {
-    //      var profileModule = new ProfileModule();
-    //      profileModule.setJob(job);
-    //      profileModule.setLogger(logger);
-    //      profileModule.setXmlUtils(xmlUtils);
-    //      profileModule.setParallel(parallel);
-    //      profileModule.setProcessingMode(processingMode);
-    //      profileModule.setFileInfoFilter((fileInfo -> Objects.equals(fileInfo.format, ATTR_FORMAT_VALUE_DITAMAP)));
-    //
-    //      profileModule.execute(input);
-    //    }
-
     return null;
   }
 
@@ -98,18 +84,9 @@ public final class MapReaderModule extends AbstractReaderModule {
       pipe.add(debugFilter);
     }
 
-    //    if (filterUtils != null) {
-    //      final ProfilingFilter profilingFilter = new ProfilingFilter();
-    //      profilingFilter.setLogger(logger);
-    //      profilingFilter.setJob(job);
-    //      profilingFilter.setFilterUtils(filterUtils);
-    //      profilingFilter.setCurrentFile(fileToParse);
-    //      pipe.add(profilingFilter);
-    //    }
-
     final ValidationFilter validationFilter = new ValidationFilter();
     validationFilter.setLogger(logger);
-    validationFilter.setValidateMap(validateMap);
+    //    validationFilter.setValidateMap(validateMap);
     validationFilter.setCurrentFile(fileToParse);
     validationFilter.setJob(job);
     validationFilter.setProcessingMode(processingMode);
@@ -127,7 +104,7 @@ public final class MapReaderModule extends AbstractReaderModule {
     listFilter.setErrorHandler(new DITAOTXMLErrorHandler(fileToParse.toString(), logger, processingMode));
     pipe.add(listFilter);
 
-    ditaWriterFilter.setDefaultValueMap(defaultValueMap);
+    //    ditaWriterFilter.setDefaultValueMap(defaultValueMap);
     ditaWriterFilter.setCurrentFile(currentFile);
     ditaWriterFilter.setOutputFile(outputFile);
     pipe.add(ditaWriterFilter);
@@ -155,42 +132,41 @@ public final class MapReaderModule extends AbstractReaderModule {
     }
     //        }
   }
-
-  @Override
-  void outputResult() throws DITAOTException {
-    super.outputResult();
-
-    try {
-      final SubjectSchemeReader subjectSchemeReader = new SubjectSchemeReader();
-      subjectSchemeReader.setLogger(logger);
-      subjectSchemeReader.setJob(job);
-      subjectSchemeReader.writeMapToXML(
-        addMapFilePrefix(listFilter.getRelationshipGrap()),
-        new File(job.tempDir, FILE_NAME_SUBJECT_RELATION)
-      );
-      subjectSchemeReader.writeMapToXML(
-        addMapFilePrefix(schemeDictionary),
-        new File(job.tempDir, FILE_NAME_SUBJECT_DICTIONARY)
-      );
-    } catch (final IOException e) {
-      throw new DITAOTException("Failed to serialize subject scheme files: " + e.getMessage(), e);
-    }
-  }
-
-  /**
-   * Convert absolute paths to relative temporary directory paths
-   * @return map with relative keys and values
-   */
-  private Map<URI, Set<URI>> addMapFilePrefix(final Map<URI, Set<URI>> map) {
-    final Map<URI, Set<URI>> res = new HashMap<>();
-    for (final Map.Entry<URI, Set<URI>> e : map.entrySet()) {
-      final URI key = e.getKey();
-      final Set<URI> newSet = new HashSet<>();
-      for (final URI file : e.getValue()) {
-        newSet.add(tempFileNameScheme.generateTempFileName(file));
-      }
-      res.put(key.equals(ROOT_URI) ? key : tempFileNameScheme.generateTempFileName(key), newSet);
-    }
-    return res;
-  }
+  //  @Override
+  //  void outputResult() throws DITAOTException {
+  //    super.outputResult();
+  //
+  //    try {
+  //      final SubjectSchemeReader subjectSchemeReader = new SubjectSchemeReader();
+  //      subjectSchemeReader.setLogger(logger);
+  //      subjectSchemeReader.setJob(job);
+  //      subjectSchemeReader.writeMapToXML(
+  //        addMapFilePrefix(listFilter.getRelationshipGrap()),
+  //        new File(job.tempDir, FILE_NAME_SUBJECT_RELATION)
+  //      );
+  //      subjectSchemeReader.writeMapToXML(
+  //        addMapFilePrefix(schemeDictionary),
+  //        new File(job.tempDir, FILE_NAME_SUBJECT_DICTIONARY)
+  //      );
+  //    } catch (final IOException e) {
+  //      throw new DITAOTException("Failed to serialize subject scheme files: " + e.getMessage(), e);
+  //    }
+  //  }
+  //
+  //  /**
+  //   * Convert absolute paths to relative temporary directory paths
+  //   * @return map with relative keys and values
+  //   */
+  //  private Map<URI, Set<URI>> addMapFilePrefix(final Map<URI, Set<URI>> map) {
+  //    final Map<URI, Set<URI>> res = new HashMap<>();
+  //    for (final Map.Entry<URI, Set<URI>> e : map.entrySet()) {
+  //      final URI key = e.getKey();
+  //      final Set<URI> newSet = new HashSet<>();
+  //      for (final URI file : e.getValue()) {
+  //        newSet.add(tempFileNameScheme.generateTempFileName(file));
+  //      }
+  //      res.put(key.equals(ROOT_URI) ? key : tempFileNameScheme.generateTempFileName(key), newSet);
+  //    }
+  //    return res;
+  //  }
 }
