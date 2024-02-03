b4a8ab1440c5d59178b413d0ddad9b106ccbdad1
Jarno Elovirta <jarno@elovirta.com>
2024-02-03 19:34:00 +0200

    Throw and log CLI exception of unsupported option
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- src/main/java/org/dita/dost/invoker/Arguments.java
+++ src/main/java/org/dita/dost/invoker/Arguments.java
@@ -9,6 +9,7 @@
 /* Derived from Apache Ant. */
 package org.dita.dost.invoker;
 
+import static org.dita.dost.invoker.Main.locale;
 import static org.dita.dost.util.LangUtils.pair;
 
 import java.io.File;
@@ -123,7 +124,7 @@ abstract class Arguments {
     } else if (isLongForm(arg, "-no-color")) {
       useColor = false;
     } else {
-      throw new BuildException("Unsupported argument: %s", arg);
+      throw new CliException(locale.getString("help.error.unsupported_argument").formatted(arg), this.getUsage(false));
     }
   }
 
