e460b500a6369d6e68d6027c6ae49c35816cb935
Jarno Elovirta <jarno@elovirta.com>
2025-03-19 19:40:57 +0200

    Change compatibility check to support nested combines
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- src/main/java/org/dita/dost/chunk/ChunkUtils.java
+++ src/main/java/org/dita/dost/chunk/ChunkUtils.java
@@ -14,9 +14,7 @@ import static org.dita.dost.util.Constants.ATTRIBUTE_NAME_CHUNK;
 import static org.dita.dost.util.Constants.MAP_TOPICREF;
 import static org.dita.dost.util.XMLUtils.getChildElements;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -40,24 +38,75 @@ public class ChunkUtils {
     if (chunkTree.isEmpty()) {
       return false;
     }
-    // Only combine without nested chunks
-    if (
-      chunkTree
-        .stream()
-        .allMatch(node -> {
-          var value = node.getValue();
-          return value.size() == 1 && value.contains(CHUNK_TO_CONTENT) && node.getChildren().isEmpty();
-        })
-    ) {
+    var tokens = chunkTree.stream().flatMap(RoseTree::flatten).flatMap(Set::stream).collect(Collectors.toSet());
+    // Only combine
+    if (tokens.size() == 1 && tokens.contains(CHUNK_TO_CONTENT)) {
       return true;
     }
     // Only split
-    var tokens = chunkTree.stream().flatMap(RoseTree::flatten).flatMap(Set::stream).collect(Collectors.toSet());
     if (tokens.size() == 1 && tokens.contains(CHUNK_BY_TOPIC)) {
+      return true;
+    }
+    if (tokens.size() == 2 && tokens.contains(CHUNK_TO_CONTENT) && tokens.contains(CHUNK_BY_TOPIC)) {
+      // No split inside combine
+      for (RoseTree<Set<String>> tree : chunkTree) {
+        if (findCombineWithSplitDescendant(tree)) {
+          return false;
+        }
+      }
       return true;
     }
     return false;
   }
+
+  public static boolean findCombineWithSplitDescendant(RoseTree<Set<String>> root) {
+    final Deque<RoseTree<Set<String>>> stack = new ArrayDeque<>();
+    stack.push(root);
+    while (!stack.isEmpty()) {
+      final RoseTree<Set<String>> node = stack.pop();
+      if (node.getValue().contains(CHUNK_TO_CONTENT)) {
+        final Stack<RoseTree<Set<String>>> descendantStack = new Stack<>();
+        descendantStack.push(node);
+        while (!descendantStack.isEmpty()) {
+          final RoseTree<Set<String>> descendant = descendantStack.pop();
+          if (descendant.getValue().contains(CHUNK_BY_TOPIC)) {
+            return true;
+          }
+          for (RoseTree<Set<String>> child : descendant.getChildren()) {
+            descendantStack.push(child);
+          }
+        }
+      }
+      for (RoseTree<Set<String>> child : node.getChildren()) {
+        stack.push(child);
+      }
+    }
+    return false;
+  }
+
+  //  public static boolean hasSplitDescendant(RoseTreeNode node) {
+  //    if (node.value == NodeType.SPLIT) {
+  //      return true;
+  //    }
+  //    for (RoseTreeNode child : node.children) {
+  //      if (hasSplitDescendant(child)) {
+  //        return true;
+  //      }
+  //    }
+  //    return false;
+  //  }
+  //
+  //  public static boolean findCombineWithSplitDescendant(RoseTree<Set<String>> node) {
+  //    if (node.getValue().equals(CHUNK_TO_CONTENT) && hasSplitDescendant(node)) {
+  //      return true;
+  //    }
+  //    for (RoseTree<Set<String>> child : node.getChildren()) {
+  //      if (findCombineWithSplitDescendant(child)) {
+  //        return true;
+  //      }
+  //    }
+  //    return false;
+  //  }
 
   private static List<RoseTree<Set<String>>> getChunkTree(Document doc) {
     List<RoseTree<Set<String>>> res = new ArrayList<>();
