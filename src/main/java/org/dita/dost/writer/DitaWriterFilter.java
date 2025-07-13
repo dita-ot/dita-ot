a5978638548a862482a630da12dbd4666338ca4a
thendarion <16006640+thendarion@users.noreply.github.com>
2025-07-13 13:55:47 +0200

    fix broken path2project in uplevels3 test
    Signed-off-by: thendarion <16006640+thendarion@users.noreply.github.com>

--- src/main/java/org/dita/dost/writer/DitaWriterFilter.java
+++ src/main/java/org/dita/dost/writer/DitaWriterFilter.java
@@ -92,12 +92,9 @@ public final class DitaWriterFilter extends AbstractXMLFilter {
   @Override
   public void startDocument() throws SAXException {
     // XXX May be require fixup
-    final URI relativeToMap = URLUtils.getRelativePath(job.getInputFile(), currentFile);
     final File path2Project = DebugAndFilterModule.getPathtoProject(
-      toFile(relativeToMap),
       toFile(currentFile),
-      toFile(job.getInputFile()),
-      job
+      toFile(job.getInputDir())
     );
     final File path2rootmap = toFile(getRelativePath(currentFile, job.getInputFile())).getParentFile();
     getContentHandler().startDocument();
@@ -110,7 +107,7 @@ public final class DitaWriterFilter extends AbstractXMLFilter {
     getContentHandler().ignorableWhitespace(new char[] { '\n' }, 0, 1);
     getContentHandler().processingInstruction(PI_WORKDIR_TARGET_URI, outputFile.toURI().resolve(".").toString());
     getContentHandler().ignorableWhitespace(new char[] { '\n' }, 0, 1);
-    if (path2Project != null) {
+    if (!path2Project.getPath().isEmpty()) {
       getContentHandler().processingInstruction(PI_PATH2PROJ_TARGET, path2Project.getPath() + File.separator);
       getContentHandler()
         .processingInstruction(PI_PATH2PROJ_TARGET_URI, toURI(path2Project).toString() + URI_SEPARATOR);
