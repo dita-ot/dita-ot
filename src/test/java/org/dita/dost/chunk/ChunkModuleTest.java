a11c15b25cf3e724b90b4c6734032ac5bcbd813b
Jarno Elovirta <jarno@elovirta.com>
2025-10-11 14:29:46 +0300

    Disable select-* support for now
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- src/test/java/org/dita/dost/chunk/ChunkModuleTest.java
+++ src/test/java/org/dita/dost/chunk/ChunkModuleTest.java
@@ -50,9 +50,10 @@ public class ChunkModuleTest extends AbstractModuleTest {
       Arguments.of("uplevels-parallel", Collections.emptyMap(), 0),
       Arguments.of("format", Collections.emptyMap(), 0),
       Arguments.of("nested", Collections.emptyMap(), 0),
-      Arguments.of("nested-select-branch", Collections.emptyMap(), 0),
-      Arguments.of("nested-select-branch-compound", Collections.emptyMap(), 0),
-      Arguments.of("nested-select-topic", Collections.emptyMap(), 0),
+      // TODO: Disable select support for now
+      //      Arguments.of("nested-select-branch", Collections.emptyMap(), 0),
+      //      Arguments.of("nested-select-branch-compound", Collections.emptyMap(), 0),
+      //      Arguments.of("nested-select-topic", Collections.emptyMap(), 0),
       Arguments.of("scope", Collections.emptyMap(), 0),
       Arguments.of("topicgroup", Collections.emptyMap(), 1),
       Arguments.of("topichead", Collections.emptyMap(), 1),
